be2f466be03d7e11d6e859617f3ec52bb9e4ddda
Dan Kolbman <dankolbman@gmail.com>
2019-02-18 18:36:05 -0500

    :recycle: Add fakes to entrypoint script

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 /app/manage.py migrate
-/app/manage.py loaddata studies
-/app/manage.py loaddata files
+pip install -r /app/dev-requirements.txt
+/app/manage.py fakestudies -n 3
+/app/manage.py fakefiles
+
+# Make an admin user
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'admin')" | python manage.py shell
 /app/manage.py runserver 0.0.0.0:8080
