a34c272cc268afbe216cfb46d79bc05a061fdb82
Dan Kolbman <dankolbman@gmail.com>
2019-02-19 14:31:23 -0500

    :recycle: Add filter classes to objects

--- creator/files/schema.py
+++ creator/files/schema.py
@@ -1,26 +1,51 @@
+import graphene
+import django_filters
 from graphene import relay, ObjectType
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
+from django_filters import OrderingFilter
 
 from .models import FileEssence, Object
 
 
-class FileNode(DjangoObjectType):
+class ObjectFilter(django_filters.FilterSet):
     class Meta:
-        model = FileEssence
-        filter_fields = ['name', 'study__name', 'study__kf_id']
-        interfaces = (relay.Node, )
+        model = Object
+        fields = []
+    order_by = OrderingFilter(fields=('created_at',))
 
 
 class ObjectNode(DjangoObjectType):
     class Meta:
         model = Object
-        filter_fields = ['key', 'version_id', 'created_at']
         interfaces = (relay.Node, )
+
+
+class FileFilter(django_filters.FilterSet):
+    class Meta:
+        model = FileEssence
+        fields = ['name', 'study__name', 'study__kf_id', 'file_type']
+
+
+class FileNode(DjangoObjectType):
+    class Meta:
+        model = FileEssence
+        interfaces = (relay.Node, )
+
+    versions = DjangoFilterConnectionField(
+        ObjectNode,
+        filterset_class=ObjectFilter,
+    )
 
 
 class Query(object):
     file_essence = relay.Node.Field(FileNode)
-    all_files = DjangoFilterConnectionField(FileNode)
+    all_files = DjangoFilterConnectionField(
+        FileNode,
+        filterset_class=FileFilter,
+    )
 
-    all_versions = DjangoFilterConnectionField(ObjectNode)
+    all_versions = DjangoFilterConnectionField(
+        ObjectNode,
+        filterset_class=ObjectFilter,
+    )
