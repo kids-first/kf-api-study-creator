4713a8f917b0a9b29b58b3a4ed3d8c9b855d5350
Dan Kolbman <dankolbman@gmail.com>
2019-02-19 13:34:51 -0500

    :recycle: Use django file field

--- creator/files/models.py
+++ creator/files/models.py
@@ -1,9 +1,6 @@
 from django.db import models
-from django.core.validators import RegexValidator
+from django.core.validators import MinValueValidator
 from creator.studies.models import Study
-
-
-KEY_RE = r'[A-Za-z0-9 !\-\_\.\*\'\(\)\/]+'
 
 
 class FileEssence(models.Model):
@@ -20,20 +17,23 @@ class FileEssence(models.Model):
 
 
 class Object(models.Model):
-    key = models.CharField(max_length=1023,
-                           help_text='The s3 key where this object is located',
-                           validators=[RegexValidator(KEY_RE)])
-    version_id = models.CharField(max_length=32,
-                                  help_text='The s3 version id of this object')
+    key = models.FileField(upload_to='uploads/',
+                           help_text=('Field to track the storage location of '
+                                      'the object'))
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      null=False,
+                                      help_text='Time the object was created')
+    size = models.BigIntegerField(
+            validators=[
+                MinValueValidator(0, 'File size must be a positive number')
+            ],
+            help_text='Size of the object in bytes')
 
     root_file = models.ForeignKey(FileEssence,
                                   related_name='versions',
                                   help_text=('The file that this version '
                                              'object belongs to'),
                                   on_delete=models.CASCADE,)
-    created_at = models.DateTimeField(auto_now_add=True,
-                                      null=False,
-                                      help_text='Time the object was created')
 
     def __str__(self):
         return (f'{self.root_file.study.kf_id} '
