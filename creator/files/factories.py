cced030bd780269ee0c6bb0672d5418d613435af
Dan Kolbman <dankolbman@gmail.com>
2019-02-19 13:36:00 -0500

    :recycle: Update factory for new fields

--- creator/files/factories.py
+++ creator/files/factories.py
@@ -1,15 +1,24 @@
 import factory
 import random
+from faker.providers import BaseProvider
 from .models import FileEssence, Object
 from creator.studies.models import Study
+
+
+class FileTypeProvider(BaseProvider):
+    def file_type(self):
+        return random.choice(['SAM', 'SHM', 'CLN', 'FAM'])
+
+
+factory.Faker.add_provider(FileTypeProvider)
 
 
 class ObjectFactory(factory.DjangoModelFactory):
     class Meta:
         model = Object 
 
-    key = factory.Faker('file_path', depth=3)
-    version_id = factory.Faker('md5')
+    key = factory.Faker('file_name')
+    size = factory.Faker('pyint')
 
 
 class FileEssenceFactory(factory.DjangoModelFactory):
@@ -19,6 +28,7 @@ class FileEssenceFactory(factory.DjangoModelFactory):
     name = factory.Faker('file_name')
     description = factory.Faker('paragraph', nb_sentences=3)
     study = factory.Iterator(Study.objects.all())
+    file_type = factory.Faker('file_type')
 
     @factory.post_generation
     def versions(self, create, extracted, **kwargs):
