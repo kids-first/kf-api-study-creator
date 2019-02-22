2df2490bd7cb08dc66f28e5a02e53335817fbf97
XuTheBunny <xzhu.fg@gmail.com>
2019-02-22 14:22:41 -0500

    ✅ Add tests for upload mutation

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -1,24 +1,65 @@
 import pytest
-from creator.schema import schema
+import json
+from creator.studies.factories import StudyFactory, BatchFactory
 
 def test_upload():
     assert True
 
-def test_study_query(client, db):
+def test_upload_query(client, db):
+    StudyFactory.create_batch(1)
+    batches = BatchFactory.create_batch(2)
     query = '''
-        {
-          allStudies {
-            edges {
-              node {
-                id
-              }
-            }
+        mutation ($file: Upload!, $batchId: Int!) {
+          createFile(file: $file, batchId: $batchId) {
+            success
           }
         }
     '''
-    query_data = {
-        "query": query.strip()
-    }
-    resp = client.post('/graphql', data=query_data, content_type='application/json')
+    with open('tests/data/manifest.txt') as f:
+        data = {
+            'operations': json.dumps({
+                'query': query.strip(),
+                'variables': {
+                    'file': None,
+                    'batchId': batches[-1].id
+                },
+            }),
+            'file': f,
+            'map': json.dumps({
+                'file': ['variables.file'],
+            }),
+        }
+        resp = client.post('/graphql', data=data)
     assert resp.status_code == 200
     assert 'data' in resp.json()
+    assert not 'errors' in resp.json()
+    assert resp.json() == {'data': {'createFile': {'success': True}}}
+    assert batches[-1].files.count() == 1
+
+def test_batch_not_exist(client, db):
+    query = '''
+        mutation ($file: Upload!, $batchId: Int!) {
+          createFile(file: $file, batchId: $batchId) {
+            success
+          }
+        }
+    '''
+    with open('tests/data/manifest.txt') as f:
+        data = {
+            'operations': json.dumps({
+                'query': query.strip(),
+                'variables': {
+                    'file': None,
+                    'batchId': 10
+                },
+            }),
+            'file': f,
+            'map': json.dumps({
+                'file': ['variables.file'],
+            }),
+        }
+        resp = client.post('/graphql', data=data)
+    assert resp.status_code == 200
+    assert 'data' in resp.json()
+    assert 'errors' in resp.json()
+    assert resp.json()['errors'][0]['message'] == 'Batch matching query does not exist.'
