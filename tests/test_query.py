a8d55d5ba423e5075c4022f56de5f58546014b77
XuTheBunny <xzhu.fg@gmail.com>
2019-02-22 11:21:10 -0500

    ✅ Add test for study queries

---
+++ tests/test_query.py
@@ -0,0 +1,41 @@
+import pytest
+from creator.studies.factories import StudyFactory
+
+def test_schema_query(client, db):
+    query = '''
+        {__schema {
+          types {
+            name
+            description
+          }
+        }}
+    '''
+    query_data = {
+        "query": query.strip()
+    }
+    resp = client.post('/graphql', data=query_data, content_type='application/json')
+    assert resp.status_code == 200
+    assert 'data' in resp.json()
+    assert '__schema' in resp.json()['data']
+
+def test_study_query(client, db):
+    studies = StudyFactory.create_batch(5)
+    query = '''
+        {
+          allStudies {
+            edges {
+              node {
+                name
+              }
+            }
+          }
+        }
+    '''
+    query_data = {
+        "query": query.strip()
+    }
+    resp = client.post('/graphql', data=query_data, content_type='application/json')
+    assert resp.status_code == 200
+    assert 'data' in resp.json()
+    assert 'allStudies' in resp.json()['data']
+    assert 5 == len(resp.json()['data']['allStudies']['edges'])
