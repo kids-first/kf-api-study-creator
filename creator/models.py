e0322ee0522be93bc122193316a1dcf030ef1fe9
Dan Kolbman <dankolbman@gmail.com>
2019-03-04 11:38:34 -0500

    :sparkles: Add manager for custom user model

--- creator/models.py
+++ creator/models.py
@@ -1,11 +1,47 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        user = self.model(
+            username=username,
+            ego_groups=[],
+            ego_roles=[],
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            username=username,
+            email=self.normalize_email(email),
+        )
+        user.is_superuser = True
+        user.ego_groups = []
+        user.ego_roles = ['ADMIN']
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractUser):
     ego_groups = ArrayField(models.CharField(max_length=100, blank=True))
     ego_roles = ArrayField(models.CharField(max_length=100, blank=True))
+
+    objects = MyUserManager()
 
     @property
     def is_admin(self):
