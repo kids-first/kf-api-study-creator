45020e48204c28b33e8f3d809eb13e01a2f606e0
XuTheBunny <xzhu.fg@gmail.com>
2019-03-08 15:17:04 -0500

    ✨ Add test for downloading authentication checking

--- creator/files/views.py
+++ creator/files/views.py
@@ -12,9 +12,8 @@ def download(request,  study_id, file_id, version_id=None):
     Only allow downloading if user is an admin or is in the study group
     """
     user = request.user
-    if user is None or not user.is_authenticated:
-        return HttpResponseNotFound('Not authenticated to download the file.')
-    if study_id not in user.ego_groups and 'ADMIN' not in user.ego_roles:
+    if (user is None or not user.is_authenticated or
+       study_id not in user.ego_groups and 'ADMIN' not in user.ego_roles):
         return HttpResponseNotFound('Not authenticated to download the file.')
 
     try:
