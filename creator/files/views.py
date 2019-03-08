fe91f3c4d2a6dbfb624f917e6ea6b6b4ee0bfb51
XuTheBunny <xzhu.fg@gmail.com>
2019-03-08 11:24:14 -0500

    ♻️ Refactor download function

--- creator/files/views.py
+++ creator/files/views.py
@@ -7,32 +7,16 @@ from django_s3_storage.storage import S3Storage
 from .models import File, Object
 
 
-# Create your views here.
-def download_latest(request, study_id, file_id):
-    try:
-        file = File.objects.get(kf_id=file_id)
-    except File.DoesNotExist:
-        return HttpResponseNotFound('No file exists with given ID')
-    obj = file.versions.latest('created_at')
-    # Need to set storage location for study bucket if using S3 backend
-    if (settings.DEFAULT_FILE_STORAGE ==
-            'django_s3_storage.storage.S3Storage'):
-        obj.key.storage = S3Storage(aws_s3_bucket_name=file.study.bucket)
-
-    response = HttpResponse(obj.key)
-    file_name = urllib.parse.quote(file.name)
-    response['Content-Disposition'] = f'attachment; filename={file_name}'
-    response['Content-Length'] = obj.size
-    return response
-
-
-def download(request, study_id, file_id, version_id):
+def download(request, study_id, file_id, version_id=None):
     try:
         file = File.objects.get(kf_id=file_id)
     except File.DoesNotExist:
         return HttpResponseNotFound('No file exists with given ID')
     try:
-        obj = file.versions.get(kf_id=version_id)
+        if version_id:
+            obj = file.versions.get(kf_id=version_id)
+        else:
+            obj = file.versions.latest('created_at')
     except Object.DoesNotExist:
         return HttpResponseNotFound('No version exists with given ID')
 
