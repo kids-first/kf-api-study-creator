709fa2abf9403ce919e77b8d2b4e0b6e3dcb1b24
Dan Kolbman <dankolbman@gmail.com>
2019-03-27 11:39:26 -0400

    :recycle: Move updateFile tests to test_mutations

--- tests/test_mutation.py
+++ tests/test_mutation.py
@@ -176,10 +176,124 @@ def test_signed_url_mutation(
         assert resp.status_code == 200
         assert resp_body["url"].startswith(f"/download/study/{study_id}/")
         assert "?token" in resp_body["url"]
-        download = client.get(resp_body['url'])
+        download = client.get(resp_body["url"])
         assert download.status_code == 200
-        assert download.content == b'aaa\nbbb\nccc\n'
+        assert download.content == b"aaa\nbbb\nccc\n"
     else:
         assert resp.status_code == 200
         assert "errors" in resp.json()
         assert resp.json()["errors"][0]["message"].startswith("Not auth")
+
+
+def test_unauthed_file_mutation_query(client, db, prep_file):
+    """
+    File mutations are not allowed without authentication
+    """
+    (_, file_id, _) = prep_file()
+    query = """
+        mutation ($kfId:String!, $description:String!, $name:String!) {
+            updateFile(kfId:$kfId, name:$name, description:$description) {
+                file { id kfId description name }
+            }
+        }
+    """
+    variables = {
+        "kfId": file_id,
+        "name": "New name",
+        "description": "New description",
+    }
+    resp = client.post(
+        "/graphql",
+        content_type="application/json",
+        data={"query": query, "variables": variables},
+    )
+    assert resp.status_code == 200
+    assert resp.json()["data"]["updateFile"] is None
+    expected_error = "Not authenticated to mutate a file."
+    assert resp.json()["errors"][0]["message"] == expected_error
+
+
+def test_my_file_mutation_query(user_client, db, prep_file):
+    """
+    File mutations are allowed on the files under the studies that
+    the user belongs to
+    """
+    (_, file_id, _) = prep_file(authed=True)
+    query = """
+        mutation ($kfId:String!, $description:String!, $name:String!) {
+            updateFile(kfId:$kfId, name:$name, description:$description) {
+                file { id kfId description name }
+            }
+        }
+    """
+    variables = {
+        "kfId": file_id,
+        "name": "New name",
+        "description": "New description",
+    }
+    resp = user_client.post(
+        "/graphql",
+        content_type="application/json",
+        data={"query": query, "variables": variables},
+    )
+    assert resp.status_code == 200
+    resp_file = resp.json()["data"]["updateFile"]["file"]
+    assert resp_file["name"] == "New name"
+    assert resp_file["description"] == "New description"
+
+
+def test_not_my_file_mutation_query(user_client, db, prep_file):
+    """
+    File mutations are not allowed on the files under the studies that
+    the user does not belong to
+    """
+    (_, file_id, _) = prep_file()
+    query = """
+        mutation ($kfId:String!, $description:String!, $name:String!) {
+            updateFile(kfId:$kfId, name:$name, description:$description) {
+                file { id kfId description name }
+            }
+        }
+    """
+    variables = {
+        "kfId": file_id,
+        "name": "New name",
+        "description": "New description",
+    }
+    resp = user_client.post(
+        "/graphql",
+        content_type="application/json",
+        data={"query": query, "variables": variables},
+    )
+    assert resp.status_code == 200
+    assert resp.json()["data"]["updateFile"] is None
+    expected_error = "Not authenticated to mutate a file."
+    assert resp.json()["errors"][0]["message"] == expected_error
+
+
+def test_admin_file_mutation_query(admin_client, db, prep_file):
+    """
+    File mutations are allowed on any files for admin user
+    """
+    (_, file_id, _) = prep_file()
+    query = """
+        mutation ($kfId:String!, $description:String!, $name:String!) {
+            updateFile(kfId:$kfId, name:$name, description:$description) {
+                file { id kfId description name }
+            }
+        }
+    """
+    variables = {
+        "kfId": file_id,
+        "name": "New name",
+        "description": "New description",
+    }
+    resp = admin_client.post(
+        "/graphql",
+        content_type="application/json",
+        data={"query": query, "variables": variables},
+    )
+    assert resp.status_code == 200
+    resp_file = resp.json()["data"]["updateFile"]["file"]
+    assert resp_file["name"] == "New name"
+    assert resp_file["description"] == "New description"
