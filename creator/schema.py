3f6a17bfd9d376c079797b357f238dc4f800b20f
Dan Kolbman <dankolbman@gmail.com>
2019-06-03 14:49:28 -0400

    :recycle: Factor out file queries
    Moves file queries into their own file within the files/schema/
    directory.

--- creator/schema.py
+++ creator/schema.py
@@ -12,14 +12,14 @@ class Query(creator.files.schema.Query,
 
 
 class Mutation(graphene.ObjectType):
-    create_file = creator.files.schema.UploadMutation.Field(
+    create_file = creator.files.schema.file.UploadMutation.Field(
         description=(
             "Upload a new file or version to an existing file and "
             "study"
         )
     )
-    update_file = creator.files.schema.FileMutation.Field()
-    delete_file = creator.files.schema.DeleteFileMutation.Field()
+    update_file = creator.files.schema.file.FileMutation.Field()
+    delete_file = creator.files.schema.file.DeleteFileMutation.Field()
     update_version = creator.files.schema.version.VersionMutation.Field()
     signed_url = creator.files.schema.SignedUrlMutation.Field()
     create_dev_token = creator.files.schema.DevDownloadTokenMutation.Field()
