8914d0bef1e496be8b0b50a1cdeb78af996a93fd
Dan Kolbman <dankolbman@gmail.com>
2020-03-24 17:57:51 -0400

    :sparkles: Add permissions in setup command

--- creator/management/commands/setup_permissions.py
+++ creator/management/commands/setup_permissions.py
@@ -1,11 +1,11 @@
 import logging
 from datetime import datetime, timedelta
 from django.core.management.base import BaseCommand, CommandError
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, Permission
 
 from creator.groups import GROUPS
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger()
 
 
 class Command(BaseCommand):
@@ -22,3 +22,13 @@ class Command(BaseCommand):
                 logging.info(f"Group {g.name} already exists")
             else:
                 logging.info(f"Group {g.name} was created")
+
+            for code in permissions:
+                logging.info(f"Adding permission {code} to group {g.name}")
+                try:
+                    p = Permission.objects.get(codename=code)
+                    g.permissions.add(p)
+                except Permission.DoesNotExist:
+                    logger.error(f"could not find permission {code}")
+                    pass
+            g.save()
