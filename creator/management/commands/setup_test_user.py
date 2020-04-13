31553ff31dc582db8fef9feda4ec6ebf3a09f871
Dan Kolbman <dankolbman@gmail.com>
2020-04-13 14:44:32 -0400

    :wrench: Add script to add test user

---
+++ creator/management/commands/setup_test_user.py
@@ -0,0 +1,29 @@
+import logging
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.db.utils import IntegrityError
+
+User = get_user_model()
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = "Setup test user"
+
+    def handle(self, *args, **options):
+        logger.info("Making test user")
+
+        try:
+            User.objects.create_superuser(
+                "testuser", "test@example.com", "test"
+            )
+        except IntegrityError:
+            logger.info("user already exists")
+        try:
+            user = User.objects.get(username="testuser")
+            user.groups.add(Group.objects.get(name="Administrators"))
+            user.save()
+        except Exception as err:
+            logger.error(f"Problem occurred adding user as admin: {err}")
