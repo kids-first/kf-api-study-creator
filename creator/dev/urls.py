c8fad8cd84a9e1fe72df1704956c4a813838a7c4
Dan Kolbman <dankolbman@gmail.com>
2020-04-13 15:18:18 -0400

    :sparkles: Add change group endpoint

--- creator/dev/urls.py
+++ creator/dev/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("reset-db/", views.reset_db, name="reset_db")]
+urlpatterns = [
+    path("reset-db/", views.reset_db, name="reset_db"),
+    path("change-groups/", views.change_groups, name="change_groups"),
+]
