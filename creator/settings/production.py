4acc5abc725bb71654d26d75124f4096d0131700
Dan Kolbman <dankolbman@gmail.com>
2020-05-28 13:05:09 -0400

    :bug: Use correct email settings

--- creator/settings/production.py
+++ creator/settings/production.py
@@ -247,7 +247,7 @@ EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
-EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL")
+EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL", False)
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
 
