f03fd9e3b72e8b2d98bf69b86862a5eeb4512ed8
Dan Kolbman <dankolbman@gmail.com>
2020-08-11 11:38:25 -0400

    :fire: Remove vault

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,19 +1,4 @@
 #!/bin/ash
-if [[ -n $VAULT_ADDR ]] && [[ -n $VAULT_ROLE ]]; then
-    vault login -method=aws role=$VAULT_ROLE 2>&1 | grep authent
-
-    # Build db connection string
-    if [[ -n $PG_HOST ]] && [[ -n $PG_NAME ]] && [[ -n $PG_SECRET ]]; then
-        echo "Load postgres connection from vault"
-        secrets=$(vault read -format=json ${PG_SECRET} | jq -c '.')
-        user=$(echo ${secrets} | jq -r '.data.user')
-        pass=$(echo ${secrets} | jq -r '.data.password')
-
-        export PG_USER=$user
-        export PG_PASS=$pass
-    fi
-fi
-
 # Try to load any database secrets, these will override the above
 if [ -n $DATABASE_SECRETS ]; then
     aws s3 cp $DATABASE_SECRETS ./database.env
