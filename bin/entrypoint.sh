70f70fddb2dac114fb214b51110a320ba0a1afbd
Alex Lubneuski <jenkins@d3b.center>
2020-08-13 15:10:48 -0400

    missing letter

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,10 +1,13 @@
-#!/bin/ash
-if [[ -z "$WORKER"]]; then
+#!/bin/bash
+if "$WORKER" ; then
+    echo "Starting worker"
     supervisord -c  /etc/supervisor/conf.d/worker.conf
 elif [[ $1 = "scheduler" ]]; then
+    echo "Starting scheduler"
     /app/manage.py schedule_jobs
     supervisord -c  /etc/supervisor/conf.d/scheduler.conf
 else
+    echo "Starting scheduler"
     python manage.py syncstudies --api $DATASERVICE_URL
     /app/manage.py migrate
     /app/manage.py setup_permissions
