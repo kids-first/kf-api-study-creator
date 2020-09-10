7c77827c2284cf056ae0fcdbf5cbe83528ec2596
XuTheBunny <xzhu.fg@gmail.com>
2020-09-10 12:21:02 -0400

    ✨ Add template attribute to file type object

--- creator/analyses/file_types.py
+++ creator/analyses/file_types.py
@@ -1,12 +1,33 @@
 FILE_TYPES = {
-    "OTH": {"name": "Other", "required_columns": []},
-    "SEQ": {"name": "Sequencing Manifest", "required_columns": []},
-    "SHM": {"name": "Shipping Manifest", "required_columns": []},
-    "CLN": {"name": "Clinical Data", "required_columns": []},
-    "DBG": {"name": "dbGaP Submission File", "required_columns": []},
-    "FAM": {"name": "Familial Relationships", "required_columns": []},
+    "OTH": {"name": "Other", "required_columns": [], "template": None},
+    "SEQ": {
+        "name": "Sequencing Manifest",
+        "required_columns": [],
+        "template": None,
+    },
+    "SHM": {
+        "name": "Shipping Manifest",
+        "required_columns": [],
+        "template": None,
+    },
+    "CLN": {
+        "name": "Clinical Data",
+        "required_columns": [],
+        "template": None,
+    },
+    "DBG": {
+        "name": "dbGaP Submission File",
+        "required_columns": [],
+        "template": None,
+    },
+    "FAM": {
+        "name": "Familial Relationships",
+        "required_columns": [],
+        "template": None,
+    },
     "S3S": {
         "name": "S3 Scrapes",
         "required_columns": ["Bucket", "Key", "Size", "ETag"],
+        "template": "s3_scrape_config.py",
     },
 }
