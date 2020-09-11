b6d52b6128bff8b40e3e79bd907a9ab37ba3c8a1
XuTheBunny <xzhu.fg@gmail.com>
2020-09-11 16:12:25 -0400

    ✅ Test for user group permission in extracking config

--- tests/files/test_extract_config.py
+++ tests/files/test_extract_config.py
@@ -51,3 +51,41 @@ def test_file_not_exist(clients, db):
     assert resp.status_code == 404
 
 
+@pytest.mark.parametrize(
+    "user_group,allowed",
+    [
+        ("Administrators", True),
+        ("Services", False),
+        ("Developers", True),
+        ("Investigators", False),
+        ("Bioinformatics", False),
+        (None, False),
+    ],
+)
+def test_config_auth(db, mocker, clients, user_group, allowed):
+    """
+    For a given user_type attempting to extract config for a file in an
+    authorized study, we expect them to be allowed/not allowed to download
+    the config file.
+    """
+    client = clients.get(user_group)
+    study = StudyFactory()
+    file = FileFactory(study=study)
+    file.file_type = 'S3S'
+    file.save()
+    version = file.versions.latest("created_at")
+    version.key = open(f"tests/data/manifest.txt")
+    mock_resp = mocker.patch("creator.files.views._resolve_version")
+    mock_resp.return_value = (file, version)
+
+    expected_name = f"attachment; filename*=UTF-8''{version.kf_id}_config.py"
+    resp = client.get(f"/extract_config/study/{study.kf_id}/file/{file.kf_id}")
+
+    if allowed:
+        assert resp.status_code == 200
+        assert resp.get("Content-Disposition") == expected_name
+        assert b"/download/study/" in resp.content
+    else:
+        assert resp.status_code == 401
+        assert resp.content == b"Not authorized to extract config for the file"
+        assert resp.get("Content-Disposition") is None
