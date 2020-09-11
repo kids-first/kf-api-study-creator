7df71348214588da093e5bf163d207033d333c2f
XuTheBunny <xzhu.fg@gmail.com>
2020-09-11 16:11:53 -0400

    ✅ Test for valid file type for extracking config

---
+++ tests/files/test_extract_config.py
@@ -0,0 +1,47 @@
+import os
+import pytest
+import json
+import boto3
+from moto import mock_s3
+from django.http.response import HttpResponse
+
+from creator.files.models import Version, File
+from creator.studies.factories import StudyFactory
+from creator.files.factories import FileFactory
+
+
+@pytest.mark.parametrize(
+    "file_type,has_config",
+    [
+        ("OTH", False),
+        ("SEQ", False),
+        ("SHM", False),
+        ("CLN", False),
+        ("DBG", False),
+        ("FAM", False),
+        ("S3S", True),
+    ],
+)
+def test_valid_file_type(clients, db, mocker, file_type, has_config):
+    client = clients.get("Administrators")
+    mock_resp = mocker.patch("creator.files.views.HttpResponse")
+    mock_resp.return_value = HttpResponse(
+        open("tests/data/s3_scrape_config_file.py")
+    )
+    client = clients.get("Administrators")
+    study = StudyFactory()
+    file = FileFactory(study=study)
+    file.file_type = file_type
+    file.save()
+    version = file.versions.latest("created_at")
+    resp = client.get(f"/extract_config/study/{study.kf_id}/file/{file.kf_id}")
+    if has_config:
+        assert resp.status_code == 200
+        assert resp.get("Content-Disposition") == (
+            f"attachment; filename*=UTF-8''" f"{version.kf_id}_config.py"
+        )
+        assert b"/download/study/" in resp.content
+    else:
+        assert resp.status_code == 404
+
+
