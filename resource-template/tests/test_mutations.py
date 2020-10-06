6cf997cd79d6c679f4a66f9536d605495a8c45e5
Dan Kolbman <dankolbman@gmail.com>
2020-10-06 14:02:23 -0400

    :white_check_mark: Add mutation tests

---
+++ resource-template/tests/test_mutations.py
@@ -0,0 +1,98 @@
+import pytest
+from graphql_relay import to_global_id
+
+from creator.{{ app_name }}.models import {{ model_name }}
+from creator.{{ app_name }}.factories import {{ model_name }}Factory
+
+
+CREATE_{{ uppercase }} = """
+mutation ($input: Create{{ model_name }}Input!) {
+    create{{ singular.title }}(input: $input) {
+        {{ singular }} {
+            id
+        }
+    }
+}
+"""
+
+UPDATE_{{ uppercase }} = """
+mutation ($id: ID!, $input: Update{{ model_name }}Input!) {
+    update{{ singular.title }}(id: $id, input: $input) {
+        {{ singular }} {
+            id
+        }
+    }
+}
+"""
+
+
+@pytest.mark.parametrize(
+    "user_group,allowed",
+    [
+        ("Administrators", True),
+        ("Services", False),
+        ("Developers", False),
+        ("Investigators", False),
+        ("Bioinformatics", False),
+        (None, False),
+    ],
+)
+def test_create_{{ singular }}(db, clients, user_group, allowed):
+    """
+    Test the create mutation.
+    """
+    client = clients.get(user_group)
+
+    resp = client.post(
+        "/graphql",
+        data={"query": CREATE_{{ uppercase }}, "variables": {"input": {"name": "Test"}}},
+        content_type="application/json",
+    )
+
+    if allowed:
+        assert (
+            resp.json()["data"]["create{{ singular.title }}"]["{{ singular }}"]
+            is not None
+        )
+    else:
+        assert resp.json()["errors"][0]["message"] == "Not allowed"
+
+
+@pytest.mark.parametrize(
+    "user_group,allowed",
+    [
+        ("Administrators", True),
+        ("Services", False),
+        ("Developers", False),
+        ("Investigators", False),
+        ("Bioinformatics", False),
+        (None, False),
+    ],
+)
+def test_update_{{ singular }}(db, clients, user_group, allowed):
+    """
+    Test the update mutation.
+    """
+    client = clients.get(user_group)
+
+    {{ singular }} = {{ model_name }}Factory()
+
+    resp = client.post(
+        "/graphql",
+        data={
+            "query": UPDATE_{{ uppercase }},
+            "variables": {
+                "id": to_global_id("{{ model_name }}Node}}", {{ singular }}.id),
+                "input": {"name": "test"},
+            },
+        },
+        content_type="application/json",
+    )
+
+    if allowed:
+        assert (
+            resp.json()["data"]["update{{ singular.title }}"]["{{ singular }}"]
+            is not None
+        )
+    else:
+        assert resp.json()["errors"][0]["message"] == "Not allowed"
