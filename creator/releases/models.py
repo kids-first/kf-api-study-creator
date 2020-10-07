8c558e6e252daae0e2cb47ff26344dc52b1987fa
Dan Kolbman <dankolbman@gmail.com>
2020-10-07 16:14:57 -0400

    :sparkles: Add ReleaseTask and ReleaseService

--- creator/releases/models.py
+++ creator/releases/models.py
@@ -7,12 +7,21 @@ from semantic_version.django_fields import VersionField
 
 from creator.fields import kf_id_generator
 from creator.studies.models import Study
+from creator.jobs.models import JobLog
 
 User = get_user_model()
 
 
 def release_id():
-    return kf_id_generator("RE")()
+    return kf_id_generator("RE")
+
+
+def task_id():
+    return kf_id_generator("TA")
+
+
+def task_service_id():
+    return kf_id_generator("TS")
 
 
 def next_version(major=False, minor=False, patch=True):
@@ -86,4 +95,105 @@ class Release(models.Model):
     )
     created_at = models.DateTimeField(
         auto_now_add=True, help_text="Date created"
+    )
+
+
+class ReleaseTask(models.Model):
+    """
+    A Release Task is a process that is run on a Task Service as part of a
+    Release.
+    """
+
+    class Meta:
+        permissions = [("list_all_releasetask", "Show all release tasks")]
+        get_latest_by = "created_at"
+
+    kf_id = models.CharField(max_length=11, primary_key=True, default=task_id)
+    uuid = models.UUIDField(
+        default=uuid.uuid4, help_text="UUID used internally"
+    )
+    state = FSMField(
+        default="waiting", help_text="The current state of the task"
+    )
+    progress = models.IntegerField(
+        default=0,
+        help_text="Optional field"
+        " representing what percentage of the task"
+        " has been completed",
+    )
+    release = models.ForeignKey(
+        Release,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        related_name="tasks",
+    )
+    release_service = models.ForeignKey(
+        "ReleaseService",
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        related_name="tasks",
+    )
+    job_log = models.ForeignKey(
+        JobLog,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="job_log",
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True, help_text="Time the task was created"
+    )
+
+
+class ReleaseService(models.Model):
+    """
+    A Release Service runs a particular Task that is required for a release
+    """
+
+    class Meta:
+        permissions = [
+            ("list_all_releaseservice", "Show all release services")
+        ]
+        get_latest_by = "created_at"
+
+    kf_id = models.CharField(
+        max_length=11,
+        primary_key=True,
+        default=task_service_id,
+        help_text="Kids First ID assigned to the service",
+    )
+    uuid = models.UUIDField(
+        default=uuid.uuid4, help_text="UUID used internally"
+    )
+    name = models.CharField(
+        max_length=100, help_text="The name of the service"
+    )
+    description = models.CharField(
+        max_length=500, help_text="Description of the service's" "function"
+    )
+    url = models.CharField(
+        max_length=200, help_text="endpoint for the service's API"
+    )
+    creator = models.ForeignKey(
+        User,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="services",
+        help_text="The user who created the service",
+    )
+    last_ok_status = models.IntegerField(
+        default=0,
+        help_text="number of pings since last"
+        " 200 response from the task's "
+        " /status endpoint",
+    )
+    enabled = models.BooleanField(
+        default=True,
+        help_text="Whether to run the task as part " "of a release.",
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True, help_text="Time the task was created"
     )
--- creator/releases/nodes.py
+++ creator/releases/nodes.py
@@ -1,8 +1,9 @@
 from graphene import relay
+from graphql_relay import from_global_id
 from graphql import GraphQLError
 from graphene_django import DjangoObjectType
 
-from creator.releases.models import Release
+from creator.releases.models import Release, ReleaseTask, ReleaseService
 
 
 class ReleaseNode(DjangoObjectType):
@@ -22,8 +23,56 @@ class ReleaseNode(DjangoObjectType):
             raise GraphQLError("Not allowed")
 
         try:
-            release = cls._meta.model.objects.get(id=id)
+            release = cls._meta.model.objects.get(pk=id)
         except cls._meta.model.DoesNotExist:
             raise GraphQLError("Releases was not found")
 
         return release
+
+
+class ReleaseTaskNode(DjangoObjectType):
+    class Meta:
+        model = ReleaseTask
+        interfaces = (relay.Node,)
+        filter_fields = ()
+
+    @classmethod
+    def get_node(cls, info, id):
+        """
+        Check permissions and return
+        """
+        user = info.context.user
+
+        if not (user.has_perm("releases.view_releasetask")):
+            raise GraphQLError("Not allowed")
+
+        try:
+            release_task = cls._meta.model.objects.get(pk=id)
+        except cls._meta.model.DoesNotExist:
+            raise GraphQLError("Release Task was not found")
+
+        return release_task
+
+
+class ReleaseServiceNode(DjangoObjectType):
+    class Meta:
+        model = ReleaseService
+        interfaces = (relay.Node,)
+        filter_fields = ()
+
+    @classmethod
+    def get_node(cls, info, id):
+        """
+        Check permissions and return
+        """
+        user = info.context.user
+
+        if not (user.has_perm("releases.view_releaseservice")):
+            raise GraphQLError("Not allowed")
+
+        try:
+            release_service = cls._meta.model.objects.get(pk=id)
+        except cls._meta.model.DoesNotExist:
+            raise GraphQLError("Release Service was not found")
+
+        return release_service
