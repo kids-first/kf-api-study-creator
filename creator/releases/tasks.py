7a0b63c4843e8d12cbb8e80fb7be4323f3fa7368
Dan Kolbman <dankolbman@gmail.com>
2020-10-28 13:28:18 -0400

    :sparkles: Update created at after save

--- creator/releases/tasks.py
+++ creator/releases/tasks.py
@@ -99,6 +99,8 @@ def sync_releases_task():
 
         if created or True:
             sync_new_release(release, r)
+            release.created_at = r["createdAt"]
+            release.save()
             new_releases += 1
 
     logger.info(
