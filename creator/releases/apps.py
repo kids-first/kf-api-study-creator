73370aedb96e99cc2cbdb59c72bc462c854b8858
Dan Kolbman <dankolbman@gmail.com>
2020-11-03 21:46:33 -0500

    :sparkles: Add release event signals

--- creator/releases/apps.py
+++ creator/releases/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class ReleasesConfig(AppConfig):
     name = "creator.releases"
+
+    def ready(self):
+        import creator.releases.signals
--- creator/releases/slack.py
+++ creator/releases/slack.py
@@ -1,8 +1,10 @@
 from slack import WebClient
 from django.conf import settings
+from creator.releases.models import Release
 
 state_emojis = {
-    "initializing": ":clock1:",
+    "waiting": ":clock1:",
+    "initializing": ":cyclone:",
     "running": ":athletic_shoe:",
     "staged": ":warning:",
     "publishing": ":recycle:",
@@ -19,10 +21,13 @@ def send_status_notification(release_id):
     """
     release = Release.objects.get(pk=release_id)
 
-    client = WebClient(token=settings.SLACK_TOKEN)
+    if settings.FEAT_SLACK_SEND_RELEASE_NOTIFICATIONS is False:
+        return
 
-    data_tracker_url = f"{settings.DATA_TRACKER_URL}"
-    release_url = f"{data_tracker_url}/releases/history/{release.pk}"
+    if release.state in ["waiting", "initializing"]:
+        return
+
+    client = WebClient(token=settings.SLACK_TOKEN)
 
     message = (
         f"🏷 *{release.kf_id}* ({release.version}) - {release.name} "
@@ -30,26 +35,81 @@ def send_status_notification(release_id):
         f"*{release.state}*"
     )
 
-    blocks = {
-        "blocks": [
-            {
-                "type": "section",
-                "text": {"type": "mrkdwn", "text": message},
-                "accessory": {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Go to Release",
-                        "emoji": false,
-                    },
-                    "style": "primary",
-                    "url": release_url,
-                    "action_id": "button-action",
-                },
-            }
-        ]
+    blocks = []
+
+    if release.state == "running":
+        blocks.extend(release_header(release))
+
+    state_block = {
+        "type": "section",
+        "text": {"type": "mrkdwn", "text": message},
     }
+    blocks.append(state_block)
 
     response = client.chat_postMessage(
         channel=settings.SLACK_RELEASE_CHANNEL, text=message, blocks=blocks
     )
+
+
+def release_header(release):
+    data_tracker_url = f"{settings.DATA_TRACKER_URL}"
+    release_url = f"{data_tracker_url}/releases/history/{release.pk}"
+
+    header = {
+        "type": "header",
+        "text": {"type": "plain_text", "text": release.name},
+    }
+
+    link_button = {
+        "type": "section",
+        "text": {"type": "mrkdwn", "text": f"> _New release!_\n"},
+        "accessory": {
+            "type": "button",
+            "text": {"type": "plain_text", "text": "Go to the Release Page"},
+            "style": "primary",
+            "url": release_url,
+            "action_id": "button-action",
+        },
+    }
+    description = {
+        "type": "section",
+        "text": {"type": "mrkdwn", "text": f"{release.description}"},
+    }
+
+    studies = {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": (
+                f"There are {release.studies.count()} studies in this "
+                "release\n"
+            ),
+        },
+    }
+    for study in release.studies.all():
+        study_url = f"{data_tracker_url}/study/{study.kf_id}/basic-info/info"
+        study_text = (
+            f"`{study.kf_id}` - "
+            f"<{study_url}|{study.short_name or study.name}>\n"
+        )
+        studies["text"]["text"] += study_text
+
+    services = {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": (
+                f"There are {release.tasks.count()} services being run "
+                "in this release\n"
+            ),
+        },
+    }
+    for task in release.tasks.all():
+        service = task.release_service
+        service_url = f"{data_tracker_url}/releases/services/{service.kf_id}"
+        service_text = (
+            f"`{service.kf_id}` - " f"<{service_url}|{service.name}>\n"
+        )
+        services["text"]["text"] += service_text
+
+    return [header, link_button, description, studies, services]
