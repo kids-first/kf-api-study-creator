68e10f57261c0f6821fcd9ac7a7fc73e998ecaa2
Natasha Singh <dukedesi22@gmail.com>
2020-11-09 18:19:55 -0500

    :white_check_mark: Test ingest run GraphQL API

--- tests/ingest_runs/test_ingest_runs_mutations.py
+++ tests/ingest_runs/test_ingest_runs_mutations.py
@@ -1,29 +1,53 @@
 import pytest
 from graphql_relay import to_global_id
-
-from creator.ingest_runs.models import IngestRun
-from creator.ingest_runs.factories import IngestRunFactory
+from creator.files.models import File
 
 
-CREATE_INGEST_REQUEST = """
-mutation ($input: CreateIngestRunInput!) {
-    createIngestRun(input: $input) {
+START_INGEST_RUN = """
+mutation ($input: StartIngestRunInput!) {
+    startIngestRun(input: $input) {
         ingestRun {
             id
+            name
+            inputHash
+            versions { edges { node { id kfId } } }
         }
     }
 }
 """
 
-UPDATE_INGEST_REQUEST = """
-mutation ($id: ID!, $input: UpdateIngestRunInput!) {
-    updateIngestRun(id: $id, input: $input) {
+CANCEL_INGEST_RUN = """
+mutation ($id: ID!) {
+    cancelIngestRun(id: $id) {
         ingestRun {
             id
+            name
+            inputHash
+            versions { edges { node { id kfId } } }
         }
     }
 }
 """
+
+
+@pytest.fixture
+def mock_ingest_job(mocker):
+    """
+    Mock ingest job
+    """
+    ingest_job = mocker.patch("creator.studies.schema.django_rq.enqueue")
+    return ingest_job
+
+
+def send_query(client, query, input_dict):
+    return client.post(
+        "/graphql",
+        data={
+            "query": query,
+            "variables": {"input": input_dict},
+        },
+        content_type="application/json",
+    )
 
 
 @pytest.mark.parametrize(
@@ -37,32 +61,52 @@ mutation ($id: ID!, $input: UpdateIngestRunInput!) {
         (None, False),
     ],
 )
-def test_create_ingest_run(db, clients, user_group, allowed):
+def test_start_ingest_run(
+    db, clients, prep_file, mock_ingest_job, user_group, allowed
+):
     """
-    Test the create mutation.
+    Test the start ingest run mutation
     """
     client = clients.get(user_group)
 
-    resp = client.post(
-        "/graphql",
-        data={
-            "query": CREATE_INGEST_REQUEST,
-            "variables": {"input": {"name": "Test"}}
-        },
-        content_type="application/json",
-    )
+    # Create a study with some files
+    for i in range(2):
+        prep_file(authed=True)
+    version_ids = [
+        to_global_id("VersionNode", f.versions.first().kf_id)
+        for f in File.objects.all()
+    ]
+
+    # Start a valid ingest runs for a batch of file versions
+    resp = send_query(client, START_INGEST_RUN, {"versions": version_ids})
 
     if allowed:
-        assert (
-            resp.json()["data"]["createIngestRun"][
-                "ingestRun"]
-            is not None
-        )
+        ir = resp.json()["data"]["startIngestRun"]["ingestRun"]
+        assert ir["name"]
+        assert ir["inputHash"]
+        for version in ir["versions"]["edges"]:
+            version["node"]["kfId"] in ir["name"]
+
     else:
         assert resp.json()["errors"][0]["message"] == "Not allowed"
 
 
 @pytest.mark.parametrize(
+    "user_group",
+    [
+        ("Administrators"),
+    ],
+)
+def test_start_ingest_run_invalid(db, clients, prep_file, user_group):
+    """
+    Ingest run with no attached versions should raise ValidationError
+    """
+    client = clients.get(user_group)
+    resp = send_query(client, START_INGEST_RUN, {"versions": []})
+    assert "at least 1 file Version" in resp.json()["errors"][0]["message"]
+
+
+@pytest.mark.parametrize(
     "user_group,allowed",
     [
         ("Administrators", True),
@@ -73,31 +117,28 @@ def test_create_ingest_run(db, clients, user_group, allowed):
         (None, False),
     ],
 )
-def test_update_ingest_run(db, clients, user_group, allowed):
+def test_cancel_ingest_run(
+    db, clients, ingest_runs, mock_ingest_job, user_group, allowed
+):
     """
-    Test the update mutation.
+    Test the cancel ingest run mutation.
     """
     client = clients.get(user_group)
 
-    ingest_run = IngestRunFactory()
+    ingest_run = ingest_runs(n=1)[0]
 
     resp = client.post(
         "/graphql",
         data={
-            "query": UPDATE_INGEST_REQUEST,
+            "query": CANCEL_INGEST_RUN,
             "variables": {
-                "id": to_global_id("IngestRunNode}}", ingest_run.id),
-                "input": {"name": "test"},
+                "id": to_global_id("IngestRunNode", ingest_run.id),
             },
         },
         content_type="application/json",
     )
 
     if allowed:
-        assert (
-            resp.json()["data"]["updateIngestRun"][
-                "ingestRun"]
-            is not None
-        )
+        assert resp.json()["data"]["cancelIngestRun"]["ingestRun"] is not None
     else:
         assert resp.json()["errors"][0]["message"] == "Not allowed"
--- tests/ingest_runs/test_ingest_runs_queries.py
+++ tests/ingest_runs/test_ingest_runs_queries.py
@@ -1,20 +1,62 @@
+from pprint import pprint
 import pytest
 from graphql_relay import to_global_id
-from creator.ingest_runs.factories import IngestRunFactory
 
-INGEST_REQUEST = """
+INGEST_RUN = """
 query ($id: ID!) {
     ingestRun(id: $id) {
         id
+        name
+        inputHash
+        versions {
+          edges {
+            node {
+              kfId
+            }
+          }
+        }
     }
 }
 """
 
-ALL_INGEST_REQUESTS = """
+ALL_INGEST_RUNS = """
 query {
     allIngestRuns {
-        edges { node { id } }
-    }
+     edges {
+       node {
+         id
+         name
+         inputHash
+         versions {
+           edges {
+             node {
+               kfId
+             }
+           }
+         }
+       }
+     }
+  }
+}
+"""
+
+FILTER_ALL_INGEST_RUNS = """
+query ($name: String!){
+    allIngestRuns(name_Icontains: $name) {
+     edges {
+       node {
+         id
+         name
+         versions {
+           edges {
+             node {
+               kfId
+             }
+           }
+         }
+       }
+     }
+  }
 }
 """
 
@@ -30,24 +72,24 @@ query {
         (None, False),
     ],
 )
-def test_query_ingest_run(db, clients, user_group, allowed):
+def test_query_ingest_run(db, clients, ingest_runs, user_group, allowed):
     client = clients.get(user_group)
 
-    ingest_run = IngestRunFactory()
+    ingest_run = ingest_runs(n=1)[0]
 
     variables = {"id": to_global_id("IngestRunNode", ingest_run.id)}
 
     resp = client.post(
         "/graphql",
-        data={"query": INGEST_REQUEST, "variables": variables},
+        data={"query": INGEST_RUN, "variables": variables},
         content_type="application/json",
     )
 
     if allowed:
-        assert (
-            resp.json()["data"]["ingestRun"]["id"] ==
-            to_global_id("IngestRunNode", ingest_run.id)
-        )
+        node = resp.json()["data"]["ingestRun"]
+        assert node["id"] == to_global_id("IngestRunNode", ingest_run.id)
+        assert node["name"]
+        assert node["versions"]
     else:
         assert resp.json()["errors"][0]["message"] == "Not allowed"
 
@@ -63,18 +105,63 @@ def test_query_ingest_run(db, clients, user_group, allowed):
         (None, False),
     ],
 )
-def test_query_all_ingest_runs(db, clients, user_group, allowed):
+def test_query_all_ingest_runs(db, clients, ingest_runs, user_group, allowed):
     client = clients.get(user_group)
 
-    ingest_run = IngestRunFactory.create_batch(5)
+    # Create an ingest run for each file
+    n = 2
+    ingest_runs(n=n)
 
     resp = client.post(
         "/graphql",
-        data={"query": ALL_INGEST_REQUESTS},
-        content_type="application/json"
+        data={"query": ALL_INGEST_RUNS},
+        content_type="application/json",
     )
 
     if allowed:
-        assert len(resp.json()["data"]["allIngestRuns"]["edges"]) == 5
+        pprint(resp.json())
+        edges = resp.json()["data"]["allIngestRuns"]["edges"]
+        assert len(edges) == n
+        for edge in edges:
+            assert "id" in edge["node"]
+            assert "name" in edge["node"]
+            assert "versions" in edge["node"]
+    else:
+        assert resp.json()["errors"][0]["message"] == "Not allowed"
+
+
+@pytest.mark.parametrize(
+    "user_group,allowed",
+    [
+        ("Administrators", True),
+        ("Services", False),
+        ("Developers", False),
+        ("Investigators", False),
+        ("Bioinformatics", False),
+        (None, False),
+    ],
+)
+def test_filter_query_all_ingest_runs(
+    db, clients, ingest_runs, user_group, allowed
+):
+    client = clients.get(user_group)
+
+    # Create an ingest run for each file
+    ingest_runs = ingest_runs(n=2)
+    version_id_filter = ingest_runs[0].versions.first().kf_id
+
+    resp = client.post(
+        "/graphql",
+        data={
+            "query": FILTER_ALL_INGEST_RUNS,
+            "variables": {"name": version_id_filter},
+        },
+        content_type="application/json",
+    )
+
+    if allowed:
+        edges = resp.json()["data"]["allIngestRuns"]["edges"]
+        assert len(edges) == 1
+        assert version_id_filter in edges[0]["node"]["name"]
     else:
         assert resp.json()["errors"][0]["message"] == "Not allowed"
