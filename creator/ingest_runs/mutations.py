b2063174d5585c7e5b283088087f0c3beb392773
Natasha Singh <dukedesi22@gmail.com>
2020-11-09 18:19:25 -0500

    :sparkles: Add initial ingest run GraphQL API

--- creator/ingest_runs/mutations.py
+++ creator/ingest_runs/mutations.py
@@ -1,29 +1,29 @@
 import graphene
+import django_rq
 from graphql import GraphQLError
 from graphql_relay import from_global_id
-
-from django.conf import settings
+from django.db import transaction
+from django.core.exceptions import ValidationError
 from creator.ingest_runs.nodes import IngestRunNode
 from creator.ingest_runs.models import IngestRun
+from creator.ingest_runs.tasks import run_ingest, cancel_ingest
+from creator.files.models import Version
 
 
-class CreateIngestRunInput(graphene.InputObjectType):
-    """ Parameters used when creating a new ingest_run """
+class StartIngestRunInput(graphene.InputObjectType):
+    """ Parameters used when starting a new ingest_run """
 
-    name = graphene.String(description="The name of the ingest_run")
-
-
-class UpdateIngestRunInput(graphene.InputObjectType):
-    """ Parameters used when updating an existing ingest_run """
-
-    name = graphene.String(description="The name of the ingest_run")
+    versions = graphene.List(
+        graphene.ID,
+        description=("File versions to be ingested"),
+    )
 
 
-class CreateIngestRunMutation(graphene.Mutation):
-    """ Creates a new ingest_run """
+class StartIngestRunMutation(graphene.Mutation):
+    """ Start a new ingest_run """
 
     class Arguments:
-        input = CreateIngestRunInput(
+        input = StartIngestRunInput(
             required=True, description="Attributes for the new ingest_run"
         )
 
@@ -31,53 +31,82 @@ class CreateIngestRunMutation(graphene.Mutation):
 
     def mutate(self, info, input):
         """
-        Creates a new ingest_run.
+        Starts a new ingest_run.
         """
         user = info.context.user
         if not user.has_perm("ingest_runs.add_ingestrun"):
             raise GraphQLError("Not allowed")
 
-        ingest_run = IngestRun()
-        return CreateIngestRunMutation(ingest_run=ingest_run)
+        # Convert relay ids to Version objects
+        version_ids = input.get("versions", [])
+        if not version_ids:
+            raise ValidationError(
+                "An IngestRun must be started with at least 1 file Version"
+            )
+
+        versions = []
+        for vid in version_ids:
+            model, vid = from_global_id(vid)
+            try:
+                versions.append(Version.objects.get(kf_id=vid))
+            except Version.DoesNotExist:
+                raise GraphQLError(f"The Version {vid} does not exist")
+
+        # Create ingest run for a set of file versions
+        with transaction.atomic():
+            ingest_run = IngestRun()
+            ingest_run.save()
+            ingest_run.versions.set(versions)
+            ingest_run.save()
+
+        uuid_str = str(ingest_run.id)
+        django_rq.enqueue(
+            run_ingest,
+            args=(uuid_str,),
+            job_id=uuid_str,
+        )
+
+        return StartIngestRunMutation(ingest_run=ingest_run)
 
 
-class UpdateIngestRunMutation(graphene.Mutation):
-    """ Update an existing ingest_run """
+class CancelIngestRunMutation(graphene.Mutation):
+    """
+    Cancel an existing ingest_run
+    """
 
     class Arguments:
         id = graphene.ID(
-            required=True, description="The ID of the ingest_run to update"
-        )
-        input = UpdateIngestRunInput(
-            required=True, description="Attributes for the ingest_run"
+            required=True, description="The ID of the ingest_run to cancel"
         )
 
     ingest_run = graphene.Field(IngestRunNode)
 
-    def mutate(self, info, id, input):
+    def mutate(self, info, id):
         """
-        Updates an existing ingest_run
+        Cancel an existing ingest_run
         """
         user = info.context.user
-        if not user.has_perm("ingest_runs.change_ingestrun"):
+        if not user.has_perm("ingest_runs.cancel_ingestrun"):
             raise GraphQLError("Not allowed")
 
-        model, node_id = from_global_id(id)
+        model, obj_id = from_global_id(id)
 
         try:
-            ingest_run = IngestRun.objects.get(id=node_id)
+            ingest_run = IngestRun.objects.get(id=obj_id)
         except IngestRun.DoesNotExist:
-            raise GraphQLError("IngestRun was not found")
+            raise GraphQLError(f"IngestRun {obj_id} was not found")
 
-        return UpdateIngestRunMutation(ingest_run=ingest_run)
+        django_rq.enqueue(cancel_ingest, args=(str(ingest_run.id),))
+
+        return CancelIngestRunMutation(ingest_run=ingest_run)
 
 
 class Mutation:
     """ Mutations for ingest_runs """
 
-    create_ingest_run = CreateIngestRunMutation.Field(
-        description="Create a new ingest_run."
+    start_ingest_run = StartIngestRunMutation.Field(
+        description="Start a new ingest_run."
     )
-    update_ingest_run = UpdateIngestRunMutation.Field(
-        description="Update a given ingest_run"
+    cancel_ingest_run = CancelIngestRunMutation.Field(
+        description="Cancel a given ingest_run"
     )
