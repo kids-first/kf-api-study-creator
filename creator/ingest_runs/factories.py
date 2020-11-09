440a9a6e82098ca4a5dae2895fe92526b4c049f1
Natasha Singh <dukedesi22@gmail.com>
2020-11-09 18:19:55 -0500

    :white_check_mark: Test ingest run GraphQL API

--- creator/ingest_runs/factories.py
+++ creator/ingest_runs/factories.py
@@ -1,12 +1,24 @@
 import pytz
 import factory
 from creator.ingest_runs.models import IngestRun
+from creator.users.factories import UserFactory
 
 
 class IngestRunFactory(factory.DjangoModelFactory):
     class Meta:
         model = IngestRun
 
+    name = factory.Faker("name")
     created_at = factory.Faker(
         "date_time_between", start_date="-2y", end_date="now", tzinfo=pytz.UTC
     )
+    creator = factory.SubFactory(UserFactory)
+
+    @factory.post_generation
+    def versions(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for version in extracted:
+                self.versions.add(version)
