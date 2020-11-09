21cda4d2c16e0ded4e36b9590d5158d87b9bc1b0
Natasha Singh <dukedesi22@gmail.com>
2020-11-09 18:18:37 -0500

    :sparkles: Add initial ingest run tasks

---
+++ creator/ingest_runs/tasks.py
@@ -0,0 +1,13 @@
+def run_ingest(ingest_run_id):
+    # TODO
+    print(f"Preparing ingest run {ingest_run_id}")
+
+
+def cancel_ingest(ingest_run_id):
+    # TODO
+    print(f"Cancelling ingest run {ingest_run_id}")
+
+
+def ingest_genomic_workflow_ouput_manifests(versions):
+    # TODO
+    print(f"Begin ingesting genomic workflow manifests: {versions}")
