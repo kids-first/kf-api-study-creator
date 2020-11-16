0cdf0632ff4ab687b8b8018f777ec79307c33c42
Natasha Singh <dukedesi22@gmail.com>
2020-11-16 10:59:59 -0500

    :wrench: Default to delete=False, if delete=True always delete job at end

--- creator/utils.py
+++ creator/utils.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 WAIT_FOR_FAIL_TIME = 0.25
 
 
-def stop_job(job_id, queue=None, delete=True):
+def stop_job(job_id, queue=None, delete=False):
     """
     Stop a running rq job and optionally delete the job from its queue
     and rq registries.
@@ -18,13 +18,11 @@ def stop_job(job_id, queue=None, delete=True):
     https://github.com/rq/rq/issues/1371 is implemented
 
     For jobs that have finished/failed, do nothing
-    For jobs that are queued/deferred cancel or delete them (see rq.job.Job)
+    For jobs that are queued/deferred remove them from their queues
     For jobs that have started, kill the worker horse executing the job
 
-    If delete is True, remove the job from its queue, all rq registries,
-    and from redis
-
-    If delete is False just remove the job from its queue
+    The delete boolean is a convenience option that when True will remove
+    the job from all registries and redis
 
     :param job_id: The id of the job: rq.Job.id
     :type job_id: str
@@ -47,29 +45,28 @@ def stop_job(job_id, queue=None, delete=True):
 
     status = redis_job.get_status()
 
-    # Skip jobs that have failed or are finished
+    # Do nothing for jobs that have failed or are finished
     if status in {JobStatus.FAILED, JobStatus.FINISHED}:
-        logger.info(f"Job {job_id} already complete, aborting")
-        return
+        logger.info(f"Job {job_id} already terminated: {status}")
 
     # Cancel jobs that haven't started yet
-    if status in {JobStatus.QUEUED, JobStatus.DEFERRED}:
+    elif status in {JobStatus.QUEUED, JobStatus.DEFERRED}:
         logger.info(
             f"Cancel job {job_id} by removing it from queue: {queue.name}"
         )
         redis_job.cancel()
-        return
-    # Kill running jobs
+
+    # Kill running job
     else:
         for worker in Worker.all(queue=queue):
-            print(worker.state, worker.get_current_job().id)
             if (worker.state == WorkerStatus.BUSY) and (
                 worker.get_current_job().id == job_id
             ):
                 logger.info(f"Killing job {job_id}")
                 send_kill_horse_command(queue.connection, worker.name)
+                break
 
-    # Delete job from failed job registry
+    # Optionally delete job from all rq job registries and redis
     if delete:
         time.sleep(WAIT_FOR_FAIL_TIME)
         logger.info(
