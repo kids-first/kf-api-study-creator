5f0e9eee209180aa959c16f093c9bb06b2170c2c
Dan Kolbman <dankolbman@gmail.com>
2020-11-16 18:07:24 -0500

    :truck: Move features to own settings file

--- creator/settings/development.py
+++ creator/settings/development.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+from creator.settings.features import *
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q$ol+cu=#pp=bgni6d7rn$+$07(!q8g_=aep0w_n+rkhy5q060'
+SECRET_KEY = "q$ol+cu=#pp=bgni6d7rn$+$07(!q8g_=aep0w_n+rkhy5q060"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,7 +33,7 @@ DEVELOPMENT_ENDPOINTS = os.environ.get("DEVELOPMENT_ENDPOINTS", False)
 
 STAGE = "dev"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 CORS_ORIGIN_ALLOW_ALL = True
 
 DEVELOP = True
@@ -39,89 +41,85 @@ DEVELOP = True
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'graphene_django',
-    'django_s3_storage',
-    'django_rq',
-    'creator.analyses',
-    'creator.dev',
-    'creator.files',
-    'creator.status',
-    'creator.studies',
-    'creator.users',
-    'creator.projects',
-    'creator.buckets',
-    'creator.email',
-    'creator.referral_tokens',
-    'creator.extract_configs',
-    'creator.jobs',
-    'creator.releases',
-    'creator.events.apps.EventsConfig',
-    'creator',
-    'corsheaders'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "graphene_django",
+    "django_s3_storage",
+    "django_rq",
+    "creator.analyses",
+    "creator.dev",
+    "creator.files",
+    "creator.status",
+    "creator.studies",
+    "creator.users",
+    "creator.projects",
+    "creator.buckets",
+    "creator.email",
+    "creator.referral_tokens",
+    "creator.extract_configs",
+    "creator.jobs",
+    "creator.releases",
+    "creator.events.apps.EventsConfig",
+    "creator",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'creator.middleware.Auth0AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "creator.middleware.Auth0AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'creator.urls'
+ROOT_URLCONF = "creator.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
         },
-    },
+    }
 ]
 
-WSGI_APPLICATION = 'creator.wsgi.application'
+WSGI_APPLICATION = "creator.wsgi.application"
 
-GRAPHENE = {
-    'SCHEMA': 'creator.schema.schema'
-}
+GRAPHENE = {"SCHEMA": "creator.schema.schema"}
 
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('PG_NAME', 'postgres'),
-        'USER': os.environ.get('PG_USER', 'postgres'),
-        'PASSWORD': os.environ.get('PG_PASS', 'postgres'),
-        'HOST': os.environ.get('PG_HOST', '127.0.0.1'),
-        'PORT': os.environ.get('PG_PORT', '5432'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("PG_NAME", "postgres"),
+        "USER": os.environ.get("PG_USER", "postgres"),
+        "PASSWORD": os.environ.get("PG_PASS", "postgres"),
+        "HOST": os.environ.get("PG_HOST", "127.0.0.1"),
+        "PORT": os.environ.get("PG_PORT", "5432"),
     }
 }
 
 
 CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-    }
+    "default": {"BACKEND": "django.core.cache.backends.locmem.LocMemCache"}
 }
 
 # Redis for RQ
@@ -183,27 +181,25 @@ if redis_pass:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"
     },
 ]
 
-AUTH_USER_MODEL = 'creator.User'
+AUTH_USER_MODEL = "creator.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -253,7 +249,7 @@ LOGGING = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
@@ -272,23 +268,24 @@ DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
 # Sets the file storage backend
 # Supports file system storage and s3 storage
-DEFAULT_FILE_STORAGE = os.environ.get('DEFAULT_FILE_STORAGE',
-                          'django.core.files.storage.FileSystemStorage')
-FILE_MAX_SIZE = 2**29
+DEFAULT_FILE_STORAGE = os.environ.get(
+    "DEFAULT_FILE_STORAGE", "django.core.files.storage.FileSystemStorage"
+)
+FILE_MAX_SIZE = 2 ** 29
 
 # Maximum time in s allowed before a token may no longer be used to download
 DOWNLOAD_TOKEN_TTL = 30
 
 # The relative path directory to upload files to when using file system storage
 # The object prefix to upload under when using S3 storage
-UPLOAD_DIR = os.environ.get('UPLOAD_DIR', 'uploads/')
+UPLOAD_DIR = os.environ.get("UPLOAD_DIR", "uploads/")
 
 # Bucket in s3 to keep logs at
-LOG_BUCKET = os.environ.get('LOG_BUCKET', 'kf-study-creator-logging')
+LOG_BUCKET = os.environ.get("LOG_BUCKET", "kf-study-creator-logging")
 # The relative path to the directory where job logs will be stored
-LOG_DIR = os.environ.get('LOG_DIR', 'logs/')
+LOG_DIR = os.environ.get("LOG_DIR", "logs/")
 
-AWS_S3_BUCKET_NAME = 'kf-study-us-east-1-dev-sd-me0owme0w'
+AWS_S3_BUCKET_NAME = "kf-study-us-east-1-dev-sd-me0owme0w"
 
 # Auth0 settings
 AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN", "https://kids-first.auth0.com")
@@ -311,132 +308,14 @@ CACHE_AUTH0_SERVICE_KEY = os.environ.get(
 )
 CACHE_AUTH0_TIMEOUT = os.environ.get("CACHE_AUTH0_TIMEOUT", 86400)
 
-CLIENT_ADMIN_SCOPE = 'role:admin'
+CLIENT_ADMIN_SCOPE = "role:admin"
 
 # User roles and groups overrides applied during auth
 USER_ROLES = os.environ.get("USER_ROLES", "")
 USER_ROLES = None if USER_ROLES == "" else USER_ROLES.split(",")
-USER_GROUPS  = os.environ.get("USER_GROUPS", "")
-USER_GROUPS = None if USER_GROUPS == "" else  USER_GROUPS.split(",")
+USER_GROUPS = os.environ.get("USER_GROUPS", "")
+USER_GROUPS = None if USER_GROUPS == "" else USER_GROUPS.split(",")
 
 # Number of seconds after which to timeout any outgoing requests
 REQUESTS_TIMEOUT = os.environ.get("REQUESTS_TIMEOUT", 30)
 REQUESTS_HEADERS = {"User-Agent": "StudyCreator/development (python-requests)"}
-
-DATASERVICE_URL = "dataservice"
-
-DATASERVICE_URL = os.environ.get("DATASERVICE_URL", "http://dataservice")
-
-COORDINATOR_URL = os.environ.get("COORDINATOR_URL", "http://coordinator:5000/graphql")
-
-CAVATICA_URL = os.environ.get(
-    "CAVATICA_URL", "https://cavatica-api.sbgenomics.com/v2"
-)
-CAVATICA_HARMONIZATION_ACCOUNT = os.environ.get(
-    "CAVATICA_HARMONIZATION_ACCOUNT", None
-)
-CAVATICA_HARMONIZATION_TOKEN = os.environ.get(
-    "CAVATICA_HARMONIZATION_TOKEN", None
-)
-CAVATICA_DELIVERY_ACCOUNT = os.environ.get("CAVATICA_DELIVERY_ACCOUNT", None)
-CAVATICA_DELIVERY_TOKEN = os.environ.get("CAVATICA_DELIVERY_TOKEN", None)
-CAVATICA_DEFAULT_WORKFLOWS = os.environ.get(
-    "CAVATICA_DEFAULT_WORKFLOWS", ""
-).split()
-
-# The project_id of the Cavatica project which will be used to clone user
-# access grants
-CAVATICA_USER_ACCESS_PROJECT = os.environ.get(
-    "CAVATICA_USER_ACCESS_PROJECT", "kids-first-drc/user-access"
-)
-
-# AWS keys used to attach volumes in Cavatica
-CAVATICA_READ_ACCESS_KEY = os.environ.get("CAVATICA_READ_ACCESS_KEY")
-CAVATICA_READ_SECRET_KEY = os.environ.get("CAVATICA_READ_SECRET_KEY")
-CAVATICA_READWRITE_ACCESS_KEY = os.environ.get("CAVATICA_READWRITE_ACCESS_KEY")
-CAVATICA_READWRITE_SECRET_KEY = os.environ.get("CAVATICA_READWRITE_SECRET_KEY")
-
-# AWS Settings for study buckets
-STUDY_BUCKETS_REGION = os.environ.get("STUDY_BUCKETS_REGION", "us-east-1")
-STUDY_BUCKETS_LOGGING_BUCKET = os.environ.get("STUDY_BUCKETS_LOGGING_BUCKET")
-STUDY_BUCKETS_DR_REGION = os.environ.get(
-    "STUDY_BUCKETS_DR_REGION", "us-west-2"
-)
-STUDY_BUCKETS_DR_LOGGING_BUCKET = os.environ.get(
-    "STUDY_BUCKETS_DR_LOGGING_BUCKET"
-)
-# Location where the study bucket inventories will be dumped
-STUDY_BUCKETS_INVENTORY_LOCATION = os.environ.get(
-    "STUDY_BUCKETS_INVENTORY_LOCATION", ""
-)
-STUDY_BUCKETS_REPLICATION_ROLE = os.environ.get(
-    "STUDY_BUCKETS_REPLICATION_ROLE"
-)
-# The prefix where bucket logs will be stored
-STUDY_BUCKETS_LOG_PREFIX = os.environ.get(
-    "STUDY_BUCKETS_LOG_PREFIX", "/studies/dev/"
-)
-
-# Slack OAuth access token
-SLACK_TOKEN = os.environ.get("SLACK_TOKEN")
-# Users to add to new channels, comma delimited, ids only
-SLACK_USERS = os.environ.get("SLACK_USERS", "").split(",")
-# Channel to post release notifications to
-SLACK_RELEASE_CHANNEL = os.environ.get(
-    "SLACK_RELEASE_CHANNEL", "#release-notifications"
-)
-
-################################################################################
-### Feature Flags
-
-# Synchronize updates to studies with dataservice
-FEAT_DATASERVICE_CREATE_STUDIES = os.environ.get(
-    "FEAT_DATASERVICE_CREATE_STUDIES", True
-)
-
-# Relay updates to studies to the dataservice
-FEAT_DATASERVICE_UPDATE_STUDIES = os.environ.get(
-    "FEAT_DATASERVICE_UPDATE_STUDIES", True
-)
-
-# Create new projects in Cavatica for each new study or existing studies
-FEAT_CAVATICA_CREATE_PROJECTS = os.environ.get(
-    "FEAT_CAVATICA_CREATE_PROJECTS", True
-)
-
-# Copy users from the CAVATICA_USER_ACCESS_PROJECT to new projects on creation
-FEAT_CAVATICA_COPY_USERS = os.environ.get("FEAT_CAVATICA_COPY_USERS", True)
-
-# Attach study buckets to new Cavatica projects on creation
-FEAT_CAVATICA_MOUNT_VOLUMES = os.environ.get(
-    "FEAT_CAVATICA_MOUNT_VOLUMES", False
-)
-# Create buckets for new studies
-FEAT_STUDY_BUCKETS_CREATE_BUCKETS = os.environ.get(
-    "FEAT_STUDY_BUCKETS_CREATE_BUCKETS", False
-)
-
-# Create Slack channel for new studies
-FEAT_SLACK_CREATE_CHANNELS = os.environ.get(
-    "FEAT_SLACK_CREATE_CHANNELS", False
-)
-
-# Whether slack notifications about releases should be sent
-FEAT_SLACK_SEND_RELEASE_NOTIFICATIONS = (
-    os.environ.get("FEAT_SLACK_SEND_RELEASE_NOTIFICATIONS", "True") == "True"
-)
-
-# How many days to expire a referral token
-REFERRAL_TOKEN_EXPIRATION_DAYS = os.environ.get(
-    'REFERRAL_TOKEN_EXPIRATION_DAYS', 3
-)
-
-# Set default from email
-DEFAULT_FROM_EMAIL = os.environ.get(
-    'DEFAULT_FROM_EMAIL', 'data-tracker@kidsfirstdrc.org'
-)
-
-# Default data tracker url
-DATA_TRACKER_URL = os.environ.get(
-    'DATA_TRACKER_URL', 'https://kf-ui-data-tracker.kidsfirstdrc.org'
-)
---
+++ creator/settings/features.py
@@ -0,0 +1,191 @@
+# FEATURES #####################################################################
+# This file contains features that may be toggled on and off and may require
+# additional configuration to work properly.
+
+import os
+
+
+# STUDIES ######################################################################
+# The Study Creator's primary purpose is to create and track studies.
+
+# New studies created in the Study Creator should also be created in the
+# Dataservice
+FEAT_DATASERVICE_CREATE_STUDIES = os.environ.get(
+    "FEAT_DATASERVICE_CREATE_STUDIES", True
+)
+
+# Any updates to studies in the Study Creator will be sent to the Dataservice
+FEAT_DATASERVICE_UPDATE_STUDIES = os.environ.get(
+    "FEAT_DATASERVICE_UPDATE_STUDIES", True
+)
+
+
+# S3 Buckets ###################################################################
+# The Study Creator needs to create and monitor S3 resources responsible for
+# storing the DRC's genomic and clinical data.
+
+# The aws region where the study buckets will be created
+STUDY_BUCKETS_REGION = os.environ.get("STUDY_BUCKETS_REGION", "us-east-1")
+
+# The name of the bucket where bucket access logs will be stored for study
+# buckets
+STUDY_BUCKETS_LOGGING_BUCKET = os.environ.get("STUDY_BUCKETS_LOGGING_BUCKET")
+
+# The prefix where bucket logs will be stored
+STUDY_BUCKETS_LOG_PREFIX = os.environ.get(
+    "STUDY_BUCKETS_LOG_PREFIX", "/studies/dev/"
+)
+
+# The aws region where data recovery buckets will be stored
+STUDY_BUCKETS_DR_REGION = os.environ.get(
+    "STUDY_BUCKETS_DR_REGION", "us-west-2"
+)
+
+# The name of the bucket where bucket access logs will be stored for data
+# recovery buckets
+STUDY_BUCKETS_DR_LOGGING_BUCKET = os.environ.get(
+    "STUDY_BUCKETS_DR_LOGGING_BUCKET"
+)
+
+# Location where the study bucket inventories will be dumped
+STUDY_BUCKETS_INVENTORY_LOCATION = os.environ.get(
+    "STUDY_BUCKETS_INVENTORY_LOCATION", ""
+)
+# The IAM role to use when configuring bucket replication
+STUDY_BUCKETS_REPLICATION_ROLE = os.environ.get(
+    "STUDY_BUCKETS_REPLICATION_ROLE"
+)
+
+
+# CAVATICA #####################################################################
+# The Study Creator can create new projects within Cavatica.
+# There are two minimal projects for each study: delivery and harmonization.
+# Each is setup in its own account.
+# Other projects, such as research or analysis projects are created in the
+# harmonization account.
+
+# The URL of the Cavatica API
+CAVATICA_URL = os.environ.get(
+    "CAVATICA_URL", "https://cavatica-api.sbgenomics.com/v2"
+)
+
+# Allow creation of new projects in Cavatica for manually for existing studies
+# and automatically for new studies
+FEAT_CAVATICA_CREATE_PROJECTS = os.environ.get(
+    "FEAT_CAVATICA_CREATE_PROJECTS", True
+)
+
+# Copy users from the CAVATICA_USER_ACCESS_PROJECT to new projects on creation
+FEAT_CAVATICA_COPY_USERS = os.environ.get("FEAT_CAVATICA_COPY_USERS", True)
+
+# Attach study buckets to new Cavatica projects on creation
+FEAT_CAVATICA_MOUNT_VOLUMES = os.environ.get(
+    "FEAT_CAVATICA_MOUNT_VOLUMES", False
+)
+
+# The name of the account to create harmonization projects in
+CAVATICA_HARMONIZATION_ACCOUNT = os.environ.get(
+    "CAVATICA_HARMONIZATION_ACCOUNT", None
+)
+# The developer token for the harmonization acconut
+CAVATICA_HARMONIZATION_TOKEN = os.environ.get(
+    "CAVATICA_HARMONIZATION_TOKEN", None
+)
+# The name of the account to create delivery projects in
+CAVATICA_DELIVERY_ACCOUNT = os.environ.get("CAVATICA_DELIVERY_ACCOUNT", None)
+# The developer token for the delivery acconut
+CAVATICA_DELIVERY_TOKEN = os.environ.get("CAVATICA_DELIVERY_TOKEN", None)
+
+# The default workflows to setup for new studies
+# Each workflow will be given its own project in Cavatcia
+CAVATICA_DEFAULT_WORKFLOWS = os.environ.get(
+    "CAVATICA_DEFAULT_WORKFLOWS", ""
+).split()
+
+# The project_id of the Cavatica project which will be used to clone user
+# access grants
+CAVATICA_USER_ACCESS_PROJECT = os.environ.get(
+    "CAVATICA_USER_ACCESS_PROJECT", "kids-first-drc/user-access"
+)
+
+# AWS keys used to attach volumes in Cavatica
+CAVATICA_READ_ACCESS_KEY = os.environ.get("CAVATICA_READ_ACCESS_KEY")
+CAVATICA_READ_SECRET_KEY = os.environ.get("CAVATICA_READ_SECRET_KEY")
+CAVATICA_READWRITE_ACCESS_KEY = os.environ.get("CAVATICA_READWRITE_ACCESS_KEY")
+CAVATICA_READWRITE_SECRET_KEY = os.environ.get("CAVATICA_READWRITE_SECRET_KEY")
+
+
+# Create buckets for new studies
+FEAT_STUDY_BUCKETS_CREATE_BUCKETS = os.environ.get(
+    "FEAT_STUDY_BUCKETS_CREATE_BUCKETS", False
+)
+
+
+# SLACK ########################################################################
+# The Study Creator can communicate with Slack to share information such as
+# daily activity or data release statuses.
+# To use Slack features, create an app with a bot user and give the Bot User's
+# OAuth access token as SLACK_TOKEN.
+
+# Slack OAuth access token for the bot user. Should begin with 'xoxb-'
+SLACK_TOKEN = os.environ.get("SLACK_TOKEN")
+
+
+# Wheather to create Slack channels for new studies
+FEAT_SLACK_CREATE_CHANNELS = os.environ.get(
+    "FEAT_SLACK_CREATE_CHANNELS", False
+)
+
+# Slack user IDs for users to add to new channels created for studies
+# User IDs should be separated by commas
+SLACK_USERS = os.environ.get("SLACK_USERS", "").split(",")
+
+
+# Whether Slack notifications about releases should be sent
+FEAT_SLACK_SEND_RELEASE_NOTIFICATIONS = (
+    os.environ.get("FEAT_SLACK_SEND_RELEASE_NOTIFICATIONS", "True") == "True"
+)
+
+# Channel to post release notifications to
+SLACK_RELEASE_CHANNEL = os.environ.get(
+    "SLACK_RELEASE_CHANNEL", "#release-notifications"
+)
+
+
+# EMAIL ########################################################################
+# The Study Creator can utilize email to send invites to new users
+# Make sure that Django's EMAIL_ settings are configured correctly to utilize
+# this feature.
+
+# Set default from email
+DEFAULT_FROM_EMAIL = os.environ.get(
+    "DEFAULT_FROM_EMAIL", "data-tracker@kidsfirstdrc.org"
+)
+
+# How many days to expire a referral token
+REFERRAL_TOKEN_EXPIRATION_DAYS = os.environ.get(
+    "REFERRAL_TOKEN_EXPIRATION_DAYS", 3
+)
+
+# Default data tracker url
+DATA_TRACKER_URL = os.environ.get(
+    "DATA_TRACKER_URL", "https://kf-ui-data-tracker.kidsfirstdrc.org"
+)
+# The url of the Dataservice API
+DATASERVICE_URL = os.environ.get("DATASERVICE_URL", "http://dataservice")
+
+# The url of the Release Coordinator API
+COORDINATOR_URL = os.environ.get(
+    "COORDINATOR_URL", "https://kf-release-coord.kidsfirstdrc.org/graphql"
+)
+
+
+# LOGGING ######################################################################
+# The Study Creator can store logs in S3 for asynchronous tasks performed by its
+# workers
+
+# The bucket where logs will be kept
+LOG_BUCKET = os.environ.get("LOG_BUCKET", "kf-study-creator-logging")
+# The relative path to the directory where job logs will be stored within the
+# log bucket
+LOG_DIR = os.environ.get("LOG_DIR", "logs/")
