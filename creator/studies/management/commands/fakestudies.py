2db6ffedea7ef65074b47db517cf5fc33d431ef6
Dan Kolbman <dankolbman@gmail.com>
2021-03-08 11:58:21 -0500

    :factory: Setup data in Dataservice for a study

--- creator/studies/management/commands/fakestudies.py
+++ creator/studies/management/commands/fakestudies.py
@@ -9,23 +9,27 @@ User = get_user_model()
 
 
 class Command(BaseCommand):
-    help = 'Make fake studies'
+    help = "Make fake studies"
 
     def add_arguments(self, parser):
         # Positional arguments
-        parser.add_argument('-n',
-                            help='number of studies to make',
-                            type=int)
+        parser.add_argument("-n", help="number of studies to make", type=int)
 
     def handle(self, *args, **options):
         import factory.random
-        factory.random.reseed_random('Fake data seed')
+
+        factory.random.reseed_random("Fake data seed")
 
         study = StudyFactory(
             kf_id="SD_ME0WME0W",
             name="Mr. Meow's Memorable Meme Emporium",
             short_name="Cat Pics",
+            visible=True,
+            deleted=False,
         )
+
+        if settings.DATASERVICE_URL:
+            self.setup_dataservice(study)
 
         user = User.objects.get(username="testuser")
         member, _ = Membership.objects.get_or_create(
@@ -33,7 +37,42 @@ class Command(BaseCommand):
         )
         member.save()
 
-        n = options.get('n')
+        n = options.get("n")
         if not n:
             n = 5
         r = StudyFactory.create_batch(n)
+
+    def setup_dataservice(self, study):
+        requests.post(
+            f"{settings.DATASERVICE_URL}/studies",
+            json={
+                "kf_id": "SD_ME0WME0W",
+                "name": study.name,
+                "external_id": study.external_id,
+                "short_name": study.short_name,
+            },
+        )
+
+        resp = requests.post(
+            f"{settings.DATASERVICE_URL}/sequencing-centers",
+            json={"kf_id": "SC_00000000", "name": "Sequencing Center"},
+        )
+
+        for i in range(10):
+            resp = requests.post(
+                f"{settings.DATASERVICE_URL}/participants",
+                json={
+                    "kf_id": f"PT_{i:08}",
+                    "external_id": f"Participant {i}",
+                    "study_id": study.kf_id,
+                },
+            )
+            resp = requests.post(
+                f"{settings.DATASERVICE_URL}/biospecimens",
+                json={
+                    "kf_id": f"BS_{i:08}",
+                    "participant_id": f"PT_{i:08}",
+                    "sequencing_center_id": "SC_00000000",
+                    "analyte_type": "DNA",
+                },
+            )
