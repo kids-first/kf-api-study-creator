16d122e1a5af8080b03750db8ae39d65b58f80fa
Natasha Singh <dukedesi22@gmail.com>
2021-04-14 15:00:14 -0400

    :sparkles: Add validation run event types

--- creator/events/models.py
+++ creator/events/models.py
@@ -7,7 +7,7 @@ from creator.files.models import File, Version
 from creator.projects.models import Project
 from creator.buckets.models import Bucket
 from creator.data_reviews.models import DataReview
-from creator.ingest_runs.models import IngestRun
+from creator.ingest_runs.models import IngestRun, ValidationRun
 
 User = get_user_model()
 
@@ -35,6 +35,10 @@ class Event(models.Model):
     event_type = models.CharField(
         max_length=6,
         choices=(
+            ("VR_STA", "Validation Run Started"),
+            ("VR_CAN", "Validation Run Canceled"),
+            ("VR_COM", "Validation Run Completed"),
+            ("VR_FAI", "Validation Run Failed"),
             ("IR_STA", "Ingest Run Started"),
             ("IR_CAN", "Ingest Run Canceled"),
             ("IR_COM", "Ingest Run Completed"),
@@ -152,4 +156,12 @@ class Event(models.Model):
         on_delete=models.SET_NULL,
         related_name="events",
         help_text="Ingest Run related to this event",
+    )
+    validation_run = models.ForeignKey(
+        ValidationRun,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="events",
+        help_text="Validation Run related to this event",
     )
