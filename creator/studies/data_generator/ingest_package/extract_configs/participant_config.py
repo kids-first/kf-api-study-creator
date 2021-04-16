7f9ba75b7eb57ff283b5dcc1810daf6ba55ffb8a
Natasha Singh <dukedesi22@gmail.com>
2021-04-16 16:07:22 -0400

    :recycle: Study generator makes data, uses extract cfgs that conform to templates

---
+++ creator/studies/data_generator/ingest_package/extract_configs/participant_config.py
@@ -0,0 +1,25 @@
+"""
+This is an extract configuration for the `Participant Demographic and
+Administrative File` template.
+
+Operations are inherited from the standard Study Creator extract configs in
+creator/extract_configs/templates
+
+The Dataservice entities that will be built from this are:
+    - participant
+"""
+
+from kf_lib_data_ingest.etl.extract.operations import keep_map
+from kf_lib_data_ingest.common.concept_schema import CONCEPT
+from creator.extract_configs.templates.participant_config import (
+    operations as shared_ops
+)
+
+source_data_url = "file://../data/bio_manifest.tsv"
+
+operations = shared_ops + [
+    keep_map(
+        in_col="KF ID Participant",
+        out_col=CONCEPT.PARTICIPANT.TARGET_SERVICE_ID,
+    ),
+]
