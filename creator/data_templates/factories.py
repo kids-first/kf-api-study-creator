0cb75444a3963725cdd9dca8dff0cdaeaba19824
Natasha Singh <dukedesi22@gmail.com>
2021-06-17 10:53:45 -0400

    :factory: Add TemplateVersion factory

--- creator/data_templates/factories.py
+++ creator/data_templates/factories.py
@@ -1,6 +1,6 @@
 import pytz
 import factory
-from creator.data_templates.models import DataTemplate
+from creator.data_templates.models import DataTemplate, TemplateVersion
 from creator.users.factories import UserFactory
 from creator.organizations.factories import OrganizationFactory
 
@@ -17,3 +17,40 @@ class DataTemplateFactory(factory.DjangoModelFactory):
     description = factory.Faker("paragraph", nb_sentences=3)
     creator = factory.SubFactory(UserFactory)
     organization = factory.SubFactory(OrganizationFactory)
+
+
+class TemplateVersionFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = TemplateVersion
+
+    created_at = factory.Faker(
+        "date_time_between", start_date="-2y", end_date="now", tzinfo=pytz.UTC
+    )
+    description = factory.Faker("paragraph", nb_sentences=3)
+    field_definitions = factory.Dict(
+        {
+            "fields": [
+                {
+                    "key": f"key_{x}",
+                    "label": f"label_{x}",
+                    "description": f"My field label_{x} is awesome",
+                    "required": "required",
+                    "default": "foo",
+                    "data_type": "string",
+                    "instructions": f"Populate label_{x} properly",
+                    "accepted_values": None
+                }
+                for x in range(2)
+            ]
+        }
+    )
+    creator = factory.SubFactory(UserFactory)
+    data_template = factory.SubFactory(DataTemplateFactory)
+
+    @factory.post_generation
+    def studies(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            self.studies.set(extracted)
