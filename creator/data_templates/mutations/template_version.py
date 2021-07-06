fc8ce537a7372640f43f539d22e0dbdd332464e0
gsantia <gs495@drexel.edu>
2021-07-06 18:08:36 -0400

    :construction: Add mutation to link template to studies

--- creator/data_templates/mutations/template_version.py
+++ creator/data_templates/mutations/template_version.py
@@ -34,6 +34,104 @@ def check_studies(study_node_ids):
     return studies
 
 
+def check_templates(template_node_ids):
+    """
+    Check if the template_versions exist and return the TemplateVersion
+    objects if they all exist. Helper function called in add/remove templates
+    to studies mutations.
+    """
+    template_ids = []
+    for tid in template_node_ids:
+        _, temp_id = from_global_id(tid)
+        template_ids.append(temp_id)
+
+    # Check all template_versions exist
+    temps = TemplateVersion.objects.filter(pk__in=template_ids).only("kf_id").all()
+    if len(temps) != len(template_ids):
+        raise GraphQLError(
+            "Failed to add/remove template versions from studies because one "
+            "or more template_versions in the input do not exist. "
+        )
+    return temps
+
+
+class AddTemplatesToStudiesInput(graphene.InputObjectType):
+    """
+    Parameters used when adding template_versions to studies.
+    """
+
+    template_versions = graphene.List(
+        graphene.ID,
+        required=True,
+        description="The template_versions to add to studies",
+    )
+    studies = graphene.List(
+        graphene.ID,
+        required=True,
+        description="The studies the template_versions should be added to",
+    )
+
+
+class AddTemplatesToStudiesMutation(graphene.Mutation):
+    """
+    Mutation to add template_versions to studies.
+    """
+
+    class Arguments:
+        input = AddTemplatesToStudiesInput(
+            required=True,
+            description="Arguments for adding the template_versions",
+        )
+
+    def mutate(self, info, input):
+        """
+        Update the studies of the template_versions.
+        """
+        user = info.context.user
+        # Check that user can make changes to studies and templates
+        if not user.has_perm("studies.change_study"):
+            raise GraphQLError("Not allowed.")
+        if not user.has_perm("data_templates.change_datatemplate"):
+            raise GraphQLError("Not allowed.")
+
+        studies = check_studies(input["studies"])
+        for study in studies:
+            if not user.studies.filter(kf_id=study.kf_id).exists():
+                raise GraphQLError(
+                    f"Not allowed - user is not a member of study "
+                    f"{study.kf_id}. "
+                )
+
+        template_versions = check_templates(input["template_versions"])
+        # Need to check permissions for each template, so loop through
+        for template_version in template_versions:
+            # User may only update template versions owned by their org
+            if not (
+                user.organizations.filter(
+                    pk=template_version.organization.pk
+                ).exists()
+            ):
+                raise GraphQLError(
+                    "Not allowed - may only update template versions for "
+                    "templates that are owned by the user's organization."
+                )
+
+            # User may only update a template version if it is not already
+            # being used in any studies
+            if template_version.released:
+                raise GraphQLError(
+                    "Not allowed - may only update templates that are not "
+                    "being used by any studies"
+                )
+
+            template_version.studies.set(studies)
+            template_version.save()
+
+        return AddTemplatesToStudiesMutation(
+            template_versions=template_versions
+        )
+
+
 class CreateTemplateVersionInput(graphene.InputObjectType):
     """Parameters used when creating a new template_version"""
 
