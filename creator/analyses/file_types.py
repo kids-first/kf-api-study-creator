a3943a1ae635eeca04427d726a7ac8dc0b4d3939
Natasha Singh <dukedesi22@gmail.com>
2021-07-20 15:27:26 -0400

    :recycle: Relax required cols for KF file types

--- creator/analyses/file_types.py
+++ creator/analyses/file_types.py
@@ -15,89 +15,56 @@ FILE_TYPES = {
         "required_columns": [
             "First Participant ID",
             "Second Participant ID",
-            "Relationship from First to Second"
+            "Relationship from First to Second",
         ],
-        "template": "complex_family_config.py"
+        "template": "complex_family_config.py",
     },
     "FTR": {
         "name": "Family Trio",
         "required_columns": [
             "Participant ID",
             "Mother Participant ID",
-            "Father Participant ID"
+            "Father Participant ID",
         ],
-        "template": "family_trio_config.py"
+        "template": "family_trio_config.py",
     },
     "PDA": {
         "name": "Participant Details",
         "required_columns": [
-            "Family ID",
             "Participant ID",
-            "dbGaP Consent Code",
             "Clinical Sex",
-            "Gender Identity",
-            "Race",
-            "Ethnicity",
-            "Age at Study Enrollment Value",
-            "Age at Study Enrollment Units",
             "Affected Status",
             "Proband",
-            "Species",
-            "Last Known Vital Status",
-            "Age at Status Value",
-            "Age at Status Units"
         ],
-        "template": "participant_config.py"
+        "template": "participant_config.py",
     },
     "GOB": {
         "name": "General Observations",
         "required_columns": [
             "Participant ID",
-            "Age at Observation Value",
-            "Age at Observation Units",
             "Observation Name",
-            "Observation Ontology Ontobee URI",
-            "Observation Code",
             "Category",
-            "Interpretation",
             "Status",
-            "Body Site Name",
-            "Body Site UBERON Code"
         ],
-        "template": "general_observations_config.py"
+        "template": "general_observations_config.py",
     },
     "PTD": {
         "name": "Participant Diseases",
         "required_columns": [
             "Participant ID",
-            "Age at Onset Value",
-            "Age at Onset Units",
-            "Age at Abatement Value",
-            "Age at Abatement Units",
             "Condition Name",
-            "Condition MONDO Code",
-            "Verification Status",
-            "Body Site Name",
-            "Body Site UBERON Code",
-            "Category"
+            "Category",
         ],
-        "template": "participant_diseases_config.py"
+        "template": "participant_diseases_config.py",
     },
     "PTP": {
         "name": "Participant Phenotypes",
         "required_columns": [
             "Participant ID",
-            "Age at Onset Value",
-            "Age at Onset Units",
-            "Age at Abatement Value",
-            "Age at Abatement Units",
             "Condition Name",
-            "Condition HPO Code",
             "Verification Status",
-            "Body Site Name",
-            "Body Site UBERON Code"
         ],
-        "template": "participant_phenotypes_config.py"
+        "template": "participant_phenotypes_config.py",
     },
     "ALM": {
         "name": "Aliquot Manifest",
@@ -105,46 +72,22 @@ FILE_TYPES = {
             "Specimen ID",
             "Aliquot ID",
             "Analyte Type",
-            "Sequencing Center"
+            "Sequencing Center",
         ],
         "template": "aliquot_manifest_config.py",
     },
     "BBM": {
         "name": "Biobank Manifest",
-        "required_columns": [
-            "Specimen ID",
-            "Aliquot ID",
-            "Analyte Type",
-            "Quantity Value",
-            "Quantity Units",
-            "Concentration Value",
-            "Concentration Units",
-            "Preservation Method",
-            "Availability Status"
-        ],
+        "required_columns": ["Specimen ID", "Aliquot ID", "Analyte Type"],
         "template": "biobank_manifest_config.py",
-
     },
     "BCM": {
         "name": "Biospecimen Collection Manifest",
         "required_columns": [
             "Participant ID",
             "Specimen ID",
-            "Consent Short Name",
-            "Consent Group",
-            "Tissue Type Name",
-            "Tissue Type NCIt Code",
             "Composition Name",
-            "Composition SNOMED CT Code",
             "Body Site Name",
-            "Body Site UBERON Code",
-            "Age at Collection Value",
-            "Age at Collection Units",
-            "Method of Sample Procurement",
-            "Ischemic Time",
-            "Ischemic Units",
-            "Origin Specimen ID",
-            "Specimen Group ID"
         ],
         "template": "biospecimen_collection_manifest_config.py",
     },
@@ -152,28 +95,15 @@ FILE_TYPES = {
         "name": "Sequencing File Manifest",
         "required_columns": [
             "Aliquot ID",
+            "Sequencing Center",
             "Sequencing Output Filepath",
-            "Sequencing Output File Hash",
-            "File Hash Algorithm",
             "Reference Genome",
             "Experiment Strategy",
-            "Experiment Date",
             "Sequencing Library Name",
-            "Instrument Model",
             "Sequencing Platform",
-            "Library Strand",
-            "Library Selection",
-            "Library Prep Kit",
-            "Library BED File Download Link",
             "Is Paired End",
-            "Expected Mean Insert Size",
-            "Expected Mean Depth",
-            "Expected Mean Read Length",
-            "Expected Total Reads",
-            "Quality Score System",
-            "Quality Score Value"
         ],
-        "template": "sequencing_manifest_config.py"
+        "template": "sequencing_manifest_config.py",
     },
     "S3S": {
         "name": "S3 Scrapes",
