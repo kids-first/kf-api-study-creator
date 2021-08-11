d608e39e2d2edc5e864e391994f7e767f4dcb0d9
Natasha Singh <dukedesi22@gmail.com>
2021-08-10 21:09:17 -0400

    :white_check_mark: Test attach template in create/update file

--- tests/files/test_create_file.py
+++ tests/files/test_create_file.py
@@ -1,4 +1,5 @@
 import pytest
+import uuid
 from django.contrib.auth import get_user_model
 from graphql_relay import to_global_id
 
@@ -8,6 +9,7 @@ from creator.files.models import Version
 
 from creator.studies.factories import StudyFactory
 from creator.files.factories import VersionFactory
+from creator.data_templates.factories import TemplateVersionFactory
 
 User = get_user_model()
 
@@ -20,6 +22,7 @@ CREATE_FILE = """
         $fileType: FileType!,
         $study: ID
         $tags: [String]
+        $templateVersion: ID
     ) {
         createFile(
           version: $version,
@@ -28,6 +31,7 @@ CREATE_FILE = """
           description: $description,
           fileType: $fileType
           tags: $tags
+          templateVersion: $templateVersion
         ) {
             file {
                 id
@@ -36,6 +40,7 @@ CREATE_FILE = """
                 description
                 fileType
                 tags
+                templateVersion { id  }
                 versions(orderBy: "-created_at") {
                     edges {
                         node {
@@ -50,6 +55,36 @@ CREATE_FILE = """
       }
     }
 """
+
+
+def test_create_file(db, clients):
+    """
+    Test the create file mutation
+    """
+    client = clients.get("Administrators")
+    study = StudyFactory()
+    version = Version(size=123)
+    version.save()
+    template_version = TemplateVersionFactory()
+
+    variables = {
+        "version": to_global_id("VersionNode", version.pk),
+        "name": "Test file",
+        "study": to_global_id("StudyNode", study.pk),
+        "description": "This is my test file",
+        "fileType": "OTH",
+        "tags": ["tag1", "tag2"],
+        "templateVersion": to_global_id(
+            "TemplateVersionNode", template_version.pk
+        )
+    }
+
+    data = {"query": CREATE_FILE, "variables": variables}
+    resp = client.post("/graphql", content_type="application/json", data=data)
+
+    result = resp.json()["data"]["createFile"]["file"]
+    assert result["id"]
+    assert result["templateVersion"]
 
 
 def test_study_does_not_exist(db, clients):
@@ -119,3 +154,32 @@ def test_version_or_study_required(db, clients):
 
     assert "errors" in resp.json()
     assert "Study must be specified" in resp.json()["errors"][0]["message"]
+
+
+def test_template_does_not_exist(db, clients):
+    """
+    Check that an error is returned if the provided template_version
+    is not found.
+    """
+    client = clients.get("Administrators")
+    study = StudyFactory()
+    version = Version(size=123)
+    version.save()
+
+    variables = {
+        "version": to_global_id("VersionNode", version.pk),
+        "name": "Test file",
+        "study": to_global_id("StudyNode", study.pk),
+        "description": "This is my test file",
+        "fileType": "OTH",
+        "tags": ["tag1", "tag2"],
+        "templateVersion": to_global_id(
+            "TemplateVersionNode", str(uuid.uuid4())
+        )
+    }
+
+    data = {"query": CREATE_FILE, "variables": variables}
+    resp = client.post("/graphql", content_type="application/json", data=data)
+
+    assert "errors" in resp.json()
+    assert "TemplateVersion" in resp.json()["errors"][0]["message"]
