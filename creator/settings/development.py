a205041c40346ebe681c6aa602c219cf51ad2f80
Natasha Singh <dukedesi22@gmail.com>
2021-09-21 14:05:27 -0400

    :wrench: Add default flatfile configuration settings

--- creator/settings/development.py
+++ creator/settings/development.py
@@ -334,3 +334,6 @@ CLIENT_ADMIN_SCOPE = "role:admin"
 # Number of seconds after which to timeout any outgoing requests
 REQUESTS_TIMEOUT = os.environ.get("REQUESTS_TIMEOUT", 30)
 REQUESTS_HEADERS = {"User-Agent": "StudyCreator/development (python-requests)"}
+
+# Flatfile
+FLATFILE_DEV_MODE = True
