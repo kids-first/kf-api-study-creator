01ce99acdf1d724e1dda5b7cac244bc08b977f2b
gsantia <gs495@drexel.edu>
2021-11-10 17:22:55 -0500

    :art: Apply black

--- creator/studies/management/commands/syncstudies.py
+++ creator/studies/management/commands/syncstudies.py
@@ -3,52 +3,54 @@ from django.core.management.base import BaseCommand, CommandError
 from creator.studies.models import Study
 
 
-API = 'https://kf-api-dataservice.kidsfirstdrc.org'
+API = "https://kf-api-dataservice.kidsfirstdrc.org"
 
 
 class Command(BaseCommand):
-    help = 'Sync studies with the dataservice'
+    help = "Sync studies with the dataservice"
 
     def add_arguments(self, parser):
-        parser.add_argument('--api',
-                            default=API,
-                            help='Address of the dataservice api',
-                            type=str)
+        parser.add_argument(
+            "--api",
+            default=API,
+            help="Address of the dataservice api",
+            type=str,
+        )
 
     def handle(self, *args, **options):
-        api = options.get('api')
-        env = 'prd'
-        if '-dev' in api:
-            env = 'dev'
-        elif '-qa' in api:
-            env = 'qa'
+        api = options.get("api")
+        env = "prd"
+        if "-dev" in api:
+            env = "dev"
+        elif "-qa" in api:
+            env = "qa"
 
-        bucket = f'kf-study-us-east-1-{env}-'
+        bucket = f"kf-study-us-east-1-{env}-"
 
-        resp = requests.get(f'{api}/studies?limit=100')
-        studies = resp.json()['results']
+        resp = requests.get(f"{api}/studies?limit=100")
+        studies = resp.json()["results"]
 
         for study in studies:
             fields = study
-            del fields['_links']
+            del fields["_links"]
 
-            if fields['name'] is None:
-                fields['name'] = ''
+            if fields["name"] is None:
+                fields["name"] = ""
             new_study, created = Study.objects.update_or_create(
-                    defaults=fields,
-                    kf_id=fields['kf_id'])
-            s3_id = fields['kf_id'].lower().replace('_', '-')
+                defaults=fields, kf_id=fields["kf_id"]
+            )
+            s3_id = fields["kf_id"].lower().replace("_", "-")
             new_study.bucket = f"{bucket}{s3_id}"
 
             # If the study was found in the dataservice, it must not be deleted
             new_study.deleted = False
             new_study.save()
             if created:
-                print('Created', study['kf_id'])
+                print("Created", study["kf_id"])
             else:
-                print('Updated', study['kf_id'])
+                print("Updated", study["kf_id"])
 
-        ds_studies = {study['kf_id'] for study in studies}
+        ds_studies = {study["kf_id"] for study in studies}
         creator_studies = {study.kf_id for study in Study.objects.all()}
         deleted_studies = creator_studies - ds_studies
 
