2f10694a2cae351a169c7a551403bd205ff44186
Natasha Singh <dukedesi22@gmail.com>
2021-11-11 17:14:20 -0500

    :sparkles: New queries to get all invent files and all upload manifest files

--- creator/storage_analyses/queries.py
+++ creator/storage_analyses/queries.py
@@ -6,7 +6,11 @@ from graphql import GraphQLError
 from django_filters import FilterSet, OrderingFilter, CharFilter
 
 from creator.storage_analyses.nodes import StorageAnalysisNode, FileAuditNode
-from creator.storage_analyses.models import StorageAnalysis, FileAudit
+from creator.storage_analyses.models import (
+    StorageAnalysis,
+    FileAudit,
+    ResultEnum
+)
 
 
 class StorageAnalysisFilter(FilterSet):
@@ -37,6 +41,16 @@ class Query(object):
         FileAuditNode,
         filterset_class=FileAuditFilter,
         description="Get all file_audits",
+    )
+    all_cloud_inventory_files = DjangoFilterConnectionField(
+        FileAuditNode,
+        filterset_class=FileAuditFilter,
+        description="Get all files in the cloud storage inventory",
+    )
+    all_upload_manifest_files = DjangoFilterConnectionField(
+        FileAuditNode,
+        filterset_class=FileAuditFilter,
+        description="Get all files in the cloud upload manifests",
     )
     all_storage_analyses = DjangoFilterConnectionField(
         StorageAnalysisNode,
@@ -69,3 +83,45 @@ class Query(object):
         return FileAudit.objects.filter(
             storage_analysis__study__in=user.studies.all()
         ).all()
+
+    def resolve_all_cloud_inventory_files(self, info, **kwargs):
+        """
+        Return all files in inventoried in cloud storage
+        """
+        user = info.context.user
+
+        if not user.has_perm("storage_analyses.list_all_storageanalysis"):
+            raise GraphQLError("Not allowed")
+
+        # Returns files found in both cloud storage inventory and upload
+        # manifests AND files found only in cloud storage inventory. This is
+        # the equivalent of returning a table of files in cloud storage
+        # inventory
+        return FileAudit.objects.filter(
+            storage_analysis__study__in=user.studies.all()
+        ).filter(result__in=[
+            ResultEnum.matched.value,
+            ResultEnum.differ.value,
+            ResultEnum.inventory_only.value,
+        ]).all()
+
+    def resolve_all_upload_manifest_files(self, info, **kwargs):
+        """
+        Return all files uploaded to cloud storage
+        """
+        user = info.context.user
+
+        if not user.has_perm("storage_analyses.list_all_storageanalysis"):
+            raise GraphQLError("Not allowed")
+
+        # Returns files found in both cloud storage inventory and upload
+        # manifests AND files found only in the upload manifests. This is the
+        # equivalent of returning an aggregate table of files from the upload
+        # manifests
+        return FileAudit.objects.filter(
+            storage_analysis__study__in=user.studies.all()
+        ).filter(result__in=[
+            ResultEnum.matched.value,
+            ResultEnum.differ.value,
+            ResultEnum.missing.value,
+        ]).all()
